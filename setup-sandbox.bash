df571007dff6c67276d774831e961e3adf7a550c
Wolf <wolf@geek.net>
2009-11-12 15:01:41 -0500

    Split setup into a common file useful locally and the sandbox wrapper that calls it

--- setup-sandbox.bash
+++ setup-sandbox.bash
@@ -5,30 +5,4 @@
 #   Git (Already done on sandobxes)
 yum install rabbitmq-server
 
-#
-# Install everything (python) into a virtual environment.
-# Maybe not important for sandboxes, but important locally.
-#
-
-sudo easy_install -U setuptools
-sudo easy_install -U virtualenv
-virtualenv --no-site-packages sandbox-env
-. sandbox-env/bin/activate
-
-easy_install ipython
-easy_install -i http://www.turbogears.org/2.1/downloads/current/index tg.devtools
-
-#
-# Install _our_ code.
-#
-git clone ssh://engr.geek.net/forge
-
-
-#
-# Install all our (formal) dependencies.
-#
-cd forge/pyforge
-python setup.py develop
-
-
-# Start up the server?
+setup-common.bash
