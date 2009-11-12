3abc5614e6052bddf4146792f130ce03f9276bfb
Wolf <wolf@zv.cx>
2009-11-12 13:49:59 -0500

    push onto rabbit

--- mail/app/handlers/amqp.py
+++ mail/app/handlers/amqp.py
@@ -1,7 +1,12 @@
 import logging
+from carrot.messaging import Publisher
+from carrot.connection import BrokerConnection
+
 from lamson.routing import route, route_like, stateless
 from config.settings import relay
 from lamson import view, queue
+
+
 
 @route("forge-list@(host)")
 #@route("(post_name)@osb\\.(host)")
@@ -9,9 +14,17 @@ from lamson import view, queue
 def POSTING(message, post_name=None, host=None):
     relay.deliver(message)
 
-    # drop the message off into the 'posts' queue for later
-    index_q = queue.Queue("run/posts")
-    index_q.push(message)
+    conn = BrokerConnection(hostname="localhost", port=5672,
+                              userid="celeryuser", password="celerypw",
+                              virtual_host="celeryvhost")
+
+    publisher = Publisher(connection=conn,
+                            exchange="forge", routing_key="mail")
+    publisher.send({"message": message})
+    publisher.close()
+
+    #index_q = queue.Queue("run/posts")
+    #index_q.push(message)
 
     return POSTING
 
