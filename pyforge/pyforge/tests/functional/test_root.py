cf26ac8a1dc1ad8e53933aa96e6a257cb1b72a65
Mark Ramm <mark@mark-ramms-macbook-pro-2.local>
2009-11-13 09:39:57 -0500

    Theoretically working functional test for pyforge -- model stuff not 100% right yet though

--- pyforge/pyforge/tests/functional/test_root.py
+++ pyforge/pyforge/tests/functional/test_root.py
@@ -18,41 +18,13 @@ from pyforge.tests import TestController
 class TestRootController(TestController):
     def test_index(self):
         response = self.app.get('/')
-        msg = 'TurboGears 2 is rapid web application development toolkit '\
-              'designed to make your life easier.'
         # You can look for specific strings:
-        assert_true(msg in response)
+        assert_true('project' in response)
         
-        # You can also access a BeautifulSoup'ed response in your tests
-        # (First run $ easy_install BeautifulSoup 
-        # and then uncomment the next two lines)  
+        #Dumb test just looks for links on the page
+        links = response.html.findAll('a')
+        print links
+        assert_true(links, "Mummy, there are no links here!")
         
-        #links = response.html.findAll('a')
-        #print links
-        #assert_true(links, "Mummy, there are no links here!")
-        
-    def test_environ(self):
-        response = self.app.get('/environ.html')
-        assert_true('The keys in the environment are: ' in response)
-
-    def test_data(self):
-        response = self.app.get('/data.html?a=1&b=2')
-        expected = """\
-<table>
-        <tr>
-            <td>a</td>
-            <td>1</td>
-        </tr>
-        <tr>
-            <td>b</td>
-            <td>2</td>
-        </tr>
-    </table>
-"""
-        assert expected in response, response
-
-    def test_data_json(self):
-        resp = self.app.get('/data.json?a=1&b=2')
-        assert '"a": "1", "b": "2"' in resp, resp
         
 
