e0e4e97a2685048726e92010a0b063dc12a3f8f6
Rick Copeland <rcopeland@corp.sourceforge.com>
2009-11-17 19:11:38 -0500

    replace hardcoded path with pkg_resources path

--- pyforge/pyforge/lib/app_globals.py
+++ pyforge/pyforge/lib/app_globals.py
@@ -5,6 +5,7 @@
 __all__ = ['Globals']
 
 from pyforge import model as M
+import pkg_resources
 
 class Globals(object):
     """Container for objects available throughout the life of the application.
@@ -16,6 +17,6 @@ class Globals(object):
 
     def __init__(self):
         """Do nothing, by default."""
-        self.pyforge_templates = '/home/rick446/src/forge/pyforge/pyforge/templates'
+        self.pyforge_templates = pkg_resources.resource_filename('pyforge', 'templates')
 
         
