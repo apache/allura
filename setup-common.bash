048642b88ea30ea24622b5dd14c7935ae980b49a
Wolf <wolf@geek.net>
2009-11-23 11:09:06 -0500

    Force Python2.6 in virtualenv; assume git clone has already happened

--- setup-common.bash
+++ setup-common.bash
@@ -2,17 +2,20 @@
 
 #
 # Install everything (python) into a virtual environment.
-# Maybe not important for sandboxes, but important locally.
+# Important in sandboxes to force Python 2.6.  Important
+# locally to keep base installs clean.
 #
 
 echo '# installing base tools'
-sudo easy_install -U setuptools
+sudo easy_install-2.6 -U setuptools
 
 echo
 echo '# setting up a virtual environment'
-sudo easy_install -U virtualenv
+sudo easy_install-2.6 -U virtualenv
 virtualenv --no-site-packages sandbox-env
 . sandbox-env/bin/activate
+
+# from here on, everything is using Python2.6 from sandbox-env
 
 echo
 echo '# installing turbogears'
@@ -23,15 +26,18 @@ easy_install beautifulsoup
 #
 # Install _our_ code.
 #
-echo
-echo '# cloning forge repo'
-git clone ssh://engr.geek.net/forge
+# echo
+# echo '# cloning forge repo'
+# git clone ssh://gitosis@engr.geek.net/forge
+# cd forge
+#
+# This already happened just to get this file to run;
+# now assume we run it in-place.
 
 
 #
 # Install all our (formal) dependencies.
 #
-cd forge
 
 echo
 echo '# installing pyforge dependencies'
