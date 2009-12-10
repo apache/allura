0ab47233bed2d4d79272f859d50bc2eb514d80a8
Jonathan T. Beard <jbeard@geek.net>
2009-12-10 15:01:20 -0500

    Change to check properly for both "real" projects and "user" projects

--- mail/app/handlers/react.py
+++ mail/app/handlers/react.py
@@ -4,21 +4,11 @@ from lamson.routing import route, route_like, stateless
 from config.settings import relay
 from lamson import view, queue
 
-#from pymongo import Connection
-
 import os
-#import sys
-#import time
-#from pkg_resources import iter_entry_points
-#from multiprocessing import Process
-#from pprint import pformat
 
 import ming
 import pylons
-#from paste.script import command
 from paste.deploy import appconfig
-#from carrot.connection import BrokerConnection
-#from carrot.messaging import Consumer
 
 from pyforge.config.environment import load_environment
 
@@ -39,32 +29,20 @@ def REACTING(message, post_name=None, loc=None, proj=None, host=None):
     pylons.g._push_object(Globals())
     ming.configure(**conf)
 
-#    connection = Connection('localhost', 27017)
-#    db = connection['projects:' + project ]
-#    collection = db['config']
-#    somevalue = collection.find()
-#    logging.debug('JTB(BASIC) prove logging works')
-#    somevalue = Project.m.find().all()
-#    somevalue = Project.m.find({"name":"test"}).one()
     try:
-        somevalue = Project.m.find({"name":proj}).one()
+        valid = Project.m.find({"name":proj, "database":"projects:"+proj}).one()
     except:
-        logging.debug('JTB(BAD) project "' + proj + '" does not exist')
+        try:
+            valid = Project.m.find({"name":proj, "database":"users:"+proj}).one()
+        except:
+            logging.debug('REACT: project "' + proj + '" does not exist as project or user')
+        else:
+            relay.deliver(message)
+            logging.debug('REACT: project "' + proj + '" exists as user!')
     else:
         relay.deliver(message)
-        logging.debug('JTB(GOOD) project "' + proj + '" exists!')
+        logging.debug('REACT: project "' + proj + '" exists as project!')
 
-
-#    try:
-#        somevalue
-#    except NameError:
-#        logging.debug('JTB(BAD) project "' + proj + '" does not exist')
-##        logging.debug('JTB(BAD) project does not exist')
-#
-#    else:
-#        relay.deliver(message)
-#        logging.debug('JTB(GOOD) project "' + proj + '" exists!')
-##        logging.debug('JTB(GOOD) project exists!')
 
 #    conn = BrokerConnection(hostname="localhost", port=5672,
 #                              userid="celeryuser", password="celerypw",
