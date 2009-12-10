b07f9a5a6c5de91804e3f68f8079a6f739c99cfd
Jonathan T. Beard <jbeard@geek.net>
2009-12-10 14:45:06 -0500

    The set of react tests for both valid and invalid destination projects

---
+++ mail/tests/handlers/react_tests.py
@@ -0,0 +1,28 @@
+from nose.tools import *
+from lamson.testing import *
+import os
+from lamson import server
+
+relay = relay(port=8823)
+client = RouterConversation("queuetester@localhost", "requests_tests")
+confirm_format = "testing-confirm-[0-9]+@"
+noreply_format = "testing-noreply@"
+
+host = "localhost"
+
+def test_react_for_existing_project():
+    """
+    Then make sure that project react messages for existing project queued properly.
+    """
+    dest_addr = "docs.index@test.%s" % host
+    client.begin()
+    client.say(dest_addr, "Test project react messages for existing project queued properly")
+
+def test_react_for_bad_project():
+    """
+    Then make sure that project react messages for non-existing project dropped properly.
+    """
+    dest_addr = "docs.index@badproject.%s" % host
+    client.begin()
+    client.say(dest_addr, "Test project react messages for non-existing project dropped properly")
+
