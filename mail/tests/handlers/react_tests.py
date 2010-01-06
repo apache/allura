f01a08b26473318b6bdf72a0ffb54a65eb478130
Jonathan T. Beard <jbeard@geek.net>
2010-01-06 16:52:07 -0500

    Modified tests to cover additional areas

--- mail/tests/handlers/react_tests.py
+++ mail/tests/handlers/react_tests.py
@@ -14,7 +14,7 @@ def test_react_for_existing_project():
     """
     Then make sure that project react messages for existing project queued properly.
     """
-    dest_addr = "wiki.index@test.%s" % host
+    dest_addr = "wiki.Beta@test.%s" % host
     client.begin()
     client.say(dest_addr, "Test project react messages for existing project queued properly")
 
@@ -22,7 +22,7 @@ def test_react_for_bad_project():
     """
     Then make sure that project react messages for non-existing project dropped properly.
     """
-    dest_addr = "wiki.index@badproject.%s" % host
+    dest_addr = "wiki.Beta@badproject.%s" % host
     client.begin()
     client.say(dest_addr, "Test project react messages for non-existing project dropped properly")
 
@@ -30,6 +30,6 @@ def test_react_for_user_project():
     """
     Then make sure that project react messages for existing user queued properly.
     """
-    dest_addr = "wiki.index@test_user2.%s" % host
+    dest_addr = "wiki.Beta@test_user2.%s" % host
     client.begin()
     client.say(dest_addr, "Test project react messages for existing user queued properly")
