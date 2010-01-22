610880dced7ac25c7266d0aa9a859b54c65bb4fe
Wolf <wolf@geek.net>
2010-01-22 09:51:39 -0500

    Add test framework to ForgeTracker

---
+++ ForgeTracker/forgetracker/tests/__init__.py
@@ -0,0 +1,42 @@
+from os import path, system
+
+from tg import config
+from paste.deploy import loadapp
+from paste.script.appinstall import SetupCommand
+from webtest import TestApp
+
+class TestController(object):
+    """
+    Base functional test case for the controllers.
+
+    The pyforge application instance (``self.app``) set up in this test
+    case (and descendants) has authentication disabled, so that developers can
+    test the protected areas independently of the :mod:`repoze.who` plugins
+    used initially. This way, authentication can be tested once and separately.
+
+    Check pyforge.tests.functional.test_authentication for the repoze.who
+    integration tests.
+
+    This is the officially supported way to test protected areas with
+    repoze.who-testutil (http://code.gustavonarea.net/repoze.who-testutil/).
+
+    """
+
+    application_under_test = 'main'
+
+    def setUp(self):
+        """Method called by nose before running each test"""
+        # Loading the application:
+        conf_dir = config.here = path.abspath(
+            path.dirname(__file__) + '/../..')
+        wsgiapp = loadapp('config:test.ini#%s' % self.application_under_test,
+                          relative_to=conf_dir)
+        self.app = TestApp(wsgiapp)
+        # Setting it up:
+        test_file = path.join(conf_dir, 'test.ini')
+        cmd = SetupCommand('setup-app')
+        cmd.run([test_file])
+
+    def tearDown(self):
+        """Method called by nose after running each test"""
+        pass
---
+++ ForgeTracker/forgetracker/tests/test_controller.py
@@ -0,0 +1,12 @@
+from urllib import urlencode
+
+from tg import config
+from nose.tools import assert_true
+
+from forgetracker.tests import TestController
+
+class TestRootController(TestController):
+
+    def test_index(self):
+        response = self.app.get('/Issues/')
+        assert_true('Welcome to ForgeTracker' in response)
