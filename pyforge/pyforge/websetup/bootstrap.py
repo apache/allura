af540091001ab1e9bf923aa1d36d670ea339658a
Wolf <wolf@geek.net>
2010-01-22 09:26:51 -0500

    To support tests, e.g., FORGE-247, install ForgeTracker on test project

--- pyforge/pyforge/websetup/bootstrap.py
+++ pyforge/pyforge/websetup/bootstrap.py
@@ -76,6 +76,7 @@ def bootstrap(command, conf, vars):
         return
     p0.install_app('hello_forge', 'hello')
     p0.install_app('Wiki', 'wiki')
+    p0.install_app('Issues', 'bug')
     app = p0.install_app('Repository', 'src')
     with pyforge.lib.helpers.push_config(c, project=p0, app=app):
         g.publish('audit', 'scm.hg.clone', dict(
