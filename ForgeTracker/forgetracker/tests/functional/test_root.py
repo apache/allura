dee47a13a907e5f441e2c441f5532600241f2367
Wolf <wolf@geek.net>
2010-02-03 10:34:47 -0500

    FORGE-246: test new comment on tracker ticket

--- ForgeTracker/forgetracker/tests/functional/test_root.py
+++ ForgeTracker/forgetracker/tests/functional/test_root.py
@@ -8,7 +8,7 @@ class TestFunctionalController(TestController):
     def test_new_ticket(self):
         response = self.app.get('/bugs/new/')
         form = response.form
-        summary = 'first sample ticket'
+        summary = 'test new ticket'
         form['summary'] = summary
         response = form.submit().follow()
         assert_true(summary in response)
@@ -18,9 +18,19 @@ class TestFunctionalController(TestController):
     def test_two_trackers(self):
         response = self.app.get('/doc_bugs/new/')
         form = response.form
-        summary = 'first sample doc_bug'
+        summary = 'test two trackers'
         form['summary'] = summary
         response = form.submit().follow()
         assert_true(summary in response)
         response = self.app.get('/bugs/')
         assert_false(summary in response)
+
+    def test_new_comment(self):
+        response = self.app.get('/bugs/new/')
+        form = response.form
+        form['summary'] = 'test new comment'
+        form.submit()
+        comment = 'comment testing new comment'
+        self.app.post('/bugs/1/comments/reply', { 'text': comment })
+        response = self.app.get('/bugs/1/')
+        assert_true(comment in response)
