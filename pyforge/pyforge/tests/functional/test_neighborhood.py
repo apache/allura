42c43d712eab3c7646e97a0e760d370b57988981
Jenny Steele <jsteele@geek.net>
2010-02-25 15:34:51 -0500

    Added a test for neighborhood icons

--- pyforge/pyforge/tests/functional/test_neighborhood.py
+++ pyforge/pyforge/tests/functional/test_neighborhood.py
@@ -1,4 +1,6 @@
 from pylons import g, c
+import os
+import pyforge
 
 from ming.orm.ormsession import ThreadLocalORMSession
 
@@ -25,6 +27,19 @@ class TestNeighborhood(TestController):
         r = self.app.post('/mozilla/_admin/update',
                           params=dict(name='Mozq1', css='', homepage='# MozQ1!\n[Root]'),
                           extra_environ=dict(username='root'))
+
+    def test_icon(self):
+        file_name = 'info.png'
+        file_path = os.path.join(pyforge.__path__[0],'public','images',file_name)
+        file_data = file(file_path).read()
+        upload = ('icon', file_name, file_data)
+
+        r = self.app.get('/mozilla/_admin/', extra_environ=dict(username='root'))
+        r = self.app.post('/mozilla/_admin/update',
+                          params=dict(name='Mozq1', css='', homepage='# MozQ1'),
+                          extra_environ=dict(username='root'), upload_files=[upload])
+        r = self.app.get('/mozilla/icon')
+        assert r.body == file_data
 
     def test_invite(self):
         r = self.app.get('/mozilla/_moderate/', extra_environ=dict(username='root'))
