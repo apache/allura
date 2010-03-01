f79af870c069ef719a15898dd574ebf1637d7e79
Jenny Steele <jsteele@geek.net>
2010-03-01 11:38:52 -0500

    Added jquery-ui theme

--- pyforge/pyforge/public/js/pyforge-base.js
+++ pyforge/pyforge/public/js/pyforge-base.js
@@ -27,3 +27,25 @@
     });
 })(jQuery);
 
+$(document).ready(function()
+{
+    $(".defaultText").focus(function(srcc)
+    {
+        if ($(this).val() == $(this)[0].title)
+        {
+            $(this).removeClass("defaultTextActive");
+            $(this).val("");
+        }
+    });
+
+    $(".defaultText").blur(function()
+    {
+        if ($(this).val() == "")
+        {
+            $(this).addClass("defaultTextActive");
+            $(this).val($(this)[0].title);
+        }
+    });
+
+    $(".defaultText").blur();
+});
