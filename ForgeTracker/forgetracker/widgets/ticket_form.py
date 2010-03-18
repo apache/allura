e62ade8a743d8b8702538a8217dbeb8b9e95ea31
Jenny Steele <jsteele@geek.net>
2010-03-18 12:13:10 -0400

    Fixed incorrect split for status/milestone names

--- ForgeTracker/forgetracker/widgets/ticket_form.py
+++ ForgeTracker/forgetracker/widgets/ticket_form.py
@@ -10,8 +10,8 @@ ticket_form = twf.TableForm('ticket_form', action='../save_ticket', children=[
   # twf.TextField('assigned_to'),
 
     twf.SingleSelectField('milestone',
-        options=lambda: model.Globals.query.get(app_config_id=c.app.config._id).milestone_names.split(',')),
+        options=lambda: model.Globals.query.get(app_config_id=c.app.config._id).milestone_names.split()),
 
     twf.SingleSelectField('status',
-        options=lambda: model.Globals.query.get(app_config_id=c.app.config._id).status_names.split(','))
+        options=lambda: model.Globals.query.get(app_config_id=c.app.config._id).status_names.split())
 ])
