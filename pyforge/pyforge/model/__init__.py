39824268150fb5b9cb86c599dad81b18f7ef7142
Jonathan T. Beard <jbeard@geek.net>
2010-03-30 19:26:28 -0400

    [#31] Add the Award and AwardGrant models to the init set

--- pyforge/pyforge/model/__init__.py
+++ pyforge/pyforge/model/__init__.py
@@ -4,7 +4,7 @@
 from .session import ProjectSession
 from .project import Theme, Neighborhood, NeighborhoodFile, Project, ProjectFile, AppConfig, SearchConfig, ScheduledMessage
 from .discuss import Discussion, Thread, PostHistory, Post, Attachment
-from .artifact import Artifact, Message, VersionedArtifact, Snapshot, ArtifactLink, nonce, Feed
+from .artifact import Artifact, Message, VersionedArtifact, Snapshot, ArtifactLink, nonce, Feed, Award, AwardGrant
 from .auth import User, ProjectRole, OpenId, EmailAddress
 from .openid_model import OpenIdStore, OpenIdAssociation, OpenIdNonce
 from .filesystem import File
