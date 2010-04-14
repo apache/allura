334627eecd13bf85e0cd9717f7fdd4df9060fb20
Gary Bernhardt <gbernhardt@geek.net>
2010-04-14 10:44:28 -0700

    Add test for tracker index action

--- ForgeTracker/forgetracker/tests/test_controller.py
+++ ForgeTracker/forgetracker/tests/test_controller.py
@@ -10,6 +10,14 @@ from pyforge.model import User
 from pylons import c
 from forgetracker.tests import TestController
 from forgetracker.model import Ticket, Globals
+from forgetracker import tracker_main
+
+
+class WithUserAndBugsApp(TestController):
+    def setUp(self):
+        super(WithUserAndBugsApp, self).setUp()
+        c.user = User.query.get(username='test_user')
+        h.set_context('test', 'bugs')
 
 
 class TestRootController(TestController):
@@ -18,12 +26,7 @@ class TestRootController(TestController):
         assert_true('ForgeTracker for' in response)
 
 
-class TestWhenSearchingWithCustomFields(TestRootController):
-    def setUp(self):
-        super(TestRootController, self).setUp()
-        c.user = User.query.get(username='test_user')
-        h.set_context('test', 'bugs')
-
+class TestWhenSearchingWithCustomFields(WithUserAndBugsApp):
     def test_that_fields_are_in_search_results(self):
         response = self._search_results()
         assert response.html.find(
@@ -36,15 +39,37 @@ class TestWhenSearchingWithCustomFields(TestRootController):
             lambda tag: (tag.name == 'th'
                          and 'Iteration Number' in str(tag)))
 
-    @patch('forgetracker.tracker_main.search_artifact')
-    def _search_results(self, search_artifact):
-        set_tracker_custom_fields([dict(name='iteration_number',
-                                        label='Iteration Number')])
-        ticket = create_ticket(summary="colors are wrong",
-                               custom_fields=dict(iteration_number='Iteration 1'))
-        make_search_return_ticket(search_artifact, ticket)
-        response = self.app.get('/bugs/search/?q=friends')
+    def _search_results(self):
+        with search_returning_colors_are_wrong_ticket():
+            response = self.app.get('/bugs/search/?q=friends')
         return response
+
+
+class TestWhenLoadingFrontPage(WithUserAndBugsApp):
+    def test_that_recent_tickets_are_shown(self):
+        with search_returning_colors_are_wrong_ticket():
+            response = tracker_main.RootController().index()
+        tickets = response['tickets']
+        assert tickets[0].summary == 'colors are wrong'
+
+
+def search_returning_colors_are_wrong_ticket():
+    ticket = create_colors_are_wrong_ticket()
+    search_artifact = Mock()
+    matches = Mock()
+    matches.docs = [dict(ticket_num_i=ticket.ticket_num)]
+    search_artifact.return_value = matches
+    return patch('forgetracker.tracker_main.search_artifact', search_artifact)
+
+
+def create_colors_are_wrong_ticket():
+    set_tracker_custom_fields([dict(name='iteration_number',
+                                    label='Iteration Number')])
+    ticket = create_ticket(summary="colors are wrong",
+                           custom_fields=dict(iteration_number='Iteration 1'))
+    ticket.commit()
+    session(ticket).flush()
+    return ticket
 
 
 def set_tracker_custom_fields(custom_fields):
@@ -60,10 +85,4 @@ def create_ticket(summary, custom_fields):
                     custom_fields=custom_fields)
     session(ticket).flush()
     return ticket
-
-
-def make_search_return_ticket(search_artifact, ticket):
-    matches = Mock()
-    matches.docs = [dict(ticket_num_i=ticket.ticket_num)]
-    search_artifact.return_value = matches
 
