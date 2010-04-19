3ed4ea6c216d4bc29eb242815902b28b93d39dbe
Max Ischenko <mischenko@geek.net>
2010-04-19 12:41:29 +0300

    [210] added h.full_url() to create absolute urls
    This is for artifacts and other site urls. A must-have for emails
    where relative urls simply won't work.
    
    I didn't find appropriate setting in .ini file to determine domain address
    and simply re-used openid.realm. I was hesitant adding new option to avoid
    breaking production configs.

--- pyforge/pyforge/lib/helpers.py
+++ pyforge/pyforge/lib/helpers.py
@@ -277,3 +277,21 @@ def render_genshi_plaintext(template_name, **template_vars):
             filepath=filepath, filename=template_name)
     stream = tt.generate(**template_vars)
     return stream.render(encoding='utf-8').decode('utf-8')
+
+import tg
+site_url = None # cannot set it just yet since tg.config is empty
+
+def full_url(url):
+    """Make absolute URL from the relative one.
+    """
+    global site_url
+    if site_url is None:
+        # XXX: add a separate tg option instead of re-using openid.realm 
+        site_url = tg.config.get('openid.realm', 'https://newforge.sf.geek.net/')
+        site_url = site_url.replace('https:', 'http:')
+        if not site_url.endswith('/'):
+            site_url += '/'
+    if url.startswith('/'):
+        url = url[1:]
+    return site_url + url
+
