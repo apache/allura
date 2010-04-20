73e52add558f675b3de7db2b05c11baecb8d0588
Gary Bernhardt <gbernhardt@geek.net>
2010-04-20 13:39:47 -0700

    Extract common setup in globals model unit test

--- ForgeTracker/forgetracker/tests/unit/test_globals_model.py
+++ ForgeTracker/forgetracker/tests/unit/test_globals_model.py
@@ -7,11 +7,14 @@ from ming.orm.ormsession import ThreadLocalORMSession
 
 
 class TestGlobalsModel(TestWithModel):
+    def setUp(self):
+        super(TestGlobalsModel, self).setUp()
+        c.project.install_app('Tickets', 'doc_bugs')
+        ThreadLocalORMSession.flush_all()
+
     def test_it_has_current_tracker_globals(self):
         bugs_globals = Globals.query.get(app_config_id=c.app.config._id)
         assert Globals.for_current_tracker() == bugs_globals
-        c.project.install_app('Tickets', 'doc_bugs')
-        ThreadLocalORMSession.flush_all()
         h.set_context('test', 'doc_bugs')
         assert Globals.for_current_tracker() != bugs_globals
 
@@ -21,8 +24,6 @@ class TestGlobalsModel(TestWithModel):
 
     def test_ticket_numbers_are_independent(self):
         assert Globals.next_ticket_num() == 1
-        c.project.install_app('Tickets', 'doc_bugs')
-        ThreadLocalORMSession.flush_all()
         h.set_context('test', 'doc_bugs')
         assert Globals.next_ticket_num() == 1
 
