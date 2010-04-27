8a682b888db6e771b83693237dfd0e6a83ce6328
Rick Copeland <rcopeland@geek.net>
2010-04-27 13:45:34 -0400

    [#244] - Add user notifications for git and hg plugins as well

--- ForgeGit/forgegit/reactors/reactors.py
+++ ForgeGit/forgegit/reactors/reactors.py
@@ -3,6 +3,7 @@ import errno, logging, os, stat, subprocess
 from pylons import c
 
 from pyforge.lib.decorators import audit, react
+from pyforge import model as M
 
 log = logging.getLogger(__name__)
 
@@ -26,3 +27,5 @@ def init(routing_key, data):
         f.write('git')
     os.chmod(magic_file, stat.S_IRUSR|stat.S_IRGRP|stat.S_IROTH)
     repo.status = 'ready'
+    M.Notification.post_user(c.user, repo, 'created',
+                             text='Repository %s created' % repo_name)
