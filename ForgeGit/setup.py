4f56871b6012b6826c4684f36e395169a8fb68db
Jonathan T. Beard <jbeard@geek.net>
2010-05-03 17:45:00 -0400

    [#329] Match production package name install

--- ForgeGit/setup.py
+++ ForgeGit/setup.py
@@ -20,7 +20,7 @@ setup(name='ForgeGit',
       install_requires=[
           # -*- Extra requirements: -*-
           'pyforge',
-          'gitpython>=0.2.0_beta1, < 0.3.0',
+          'GitPython>=0.2.0_beta1, < 0.3.0',
       ],
       entry_points="""
       # -*- Entry points: -*-
