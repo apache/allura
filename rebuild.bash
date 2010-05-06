20ae8c1a3a1527fc5349e16a34c94eff8b578662
Jonathan T. Beard <jbeard@geek.net>
2010-05-06 17:04:00 -0400

    Tool for hand setups

---
+++ rebuild.bash
@@ -0,0 +1,32 @@
+echo '# setup pyforge...'
+pushd pyforge
+python setup.py develop
+popd
+echo '# setup ForgeDiscussion...'
+pushd ForgeDiscussion
+python setup.py develop
+popd
+echo '# setup ForgeGit...'
+pushd ForgeGit
+python setup.py develop
+popd
+echo '# setup ForgeHg...'
+pushd ForgeHg
+python setup.py develop
+popd
+echo '# setup ForgeMail...'
+pushd ForgeMail
+python setup.py develop
+popd
+echo '# setup ForgeTracker...'
+pushd ForgeTracker
+python setup.py develop
+popd
+echo '# setup ForgeWiki...'
+pushd ForgeWiki
+python setup.py develop
+popd
+echo '# setup HelloForge...'
+pushd HelloForge
+python setup.py develop
+popd
