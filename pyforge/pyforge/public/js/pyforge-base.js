a210f5ffe19c34cf65940d38c401e437cfe8860a
Jenny Steele <jsteele@geek.net>
2010-05-12 16:13:01 -0400

    [#359] Changed hover edit to click edit with new styling

--- pyforge/pyforge/public/js/pyforge-base.js
+++ pyforge/pyforge/public/js/pyforge-base.js
@@ -13,42 +13,46 @@
         $(window.location.hash + '.title-pane').removeClass('closed');
     }
 
-    var defocus = function(ele){
-        ele.closest('.editable')
-                  .addClass('viewing')
-                  .removeClass('editing');
-        ele.find('input, select, textarea').blur();
-    }
     // Setup editable widgets
     $('div.editable, span.editable, h1.editable')
-        .find('.viewer').mouseenter(function(e){
-            $(this).closest('.editable')
-                   .addClass('editing')
-                   .removeClass('viewing');
-        }).end()
-        .find('.editor').mouseleave(function(e){
-            defocus($(this));
+        .find('.viewer')
+        .append('<button class="edit_btn ui-button ui-widget ui-state-default ui-button-icon-only" title="Edit"><span class="ui-button-icon-primary ui-icon ui-icon-pencil"></span><span class="ui-button-text">Button with icon only</span></button>')
+        .click(function(e){
+            var editable = $(this).closest('.editable');
+            var editor = editable.find('.editor');
+            var viewer = editable.find('.viewer');
+            if(editor.hasClass('overlap')){
+                editor.width(viewer.width());
+            }
+            editable.addClass('editing')
+                    .removeClass('viewing')
+            return false;
         })
-        .find('input, select, textarea').focus(function(e){
-            $(this).closest('.editor').unbind('mouseleave');
-            if(this.tagName.toLowerCase() != 'textarea'){
-                $(this).keyup(function(e){
-                    if(e.keyCode == 13){
-                        $(this).closest('form').submit();
-                    }
-                });
+        .find('a').click(function(event){
+          event.stopPropagation();
+        })
+        .end()
+        .end()
+        .find('.editor')
+        .find('input, select, textarea').each(function(){
+            var editor = $(this).closest('.editor');
+            var save_btns = $('<div class="save_holder"><input type="submit" value="Save" class="ui-button ui-widget ui-state-default ui-button-text-only"/> <a href="#" class="cancel_btn">Cancel</a></div>')
+            if(editor.hasClass('multiline')){
+                editor.append(save_btns);
+            }
+            else{
+                editor.append($('<table class="holder_table"><tr/></table>')
+                            .append($('<td/>').append($(this)))
+                            .append($('<td class="save_controls"/>').append($(save_btns)))
+                );
             }
         })
-        .change(function(e){
-            $(this).closest('form').submit();
-        })
-        .blur(function(e){
-            $(this).closest('.editable').find('.editor').mouseleave(function(e){
-                defocus($(this));
-            })
-            .end()
-            .addClass('viewing')
-            .removeClass('editing');
+        .end()
+        .find('.cancel_btn').click(function(e){
+            $(this).closest('.editable')
+                      .addClass('viewing')
+                      .removeClass('editing');
+            return false;
         });
 })(jQuery);
 
