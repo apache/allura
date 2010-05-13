8d5d2614b0af8c2b47b1eaf15b2a171600bf3498
Gary Bernhardt <gbernhardt@geek.net>
2010-05-13 14:37:53 -0700

    Add build_virtualenv.py script

---
+++ build_virtualenv.py
@@ -0,0 +1,114 @@
+from os.path import exists
+import sys
+import os
+from os import symlink
+from os.path import join
+from distutils.sysconfig import get_python_lib
+from subprocess import check_call
+from subprocess import call as unchecked_call
+
+
+ENV_DIR = 'env'
+REQUIREMENTS_FILE = 'requirements.txt'
+
+
+# If the PYTHONDONTWRITEBYTECODE variable is set, it confuses easy_install
+# into installing pip as a zipped egg, which breaks pip as of 0.7.1. A fix
+# should be in the next release.
+if 'PYTHONDONTWRITEBYTECODE' in os.environ:
+    del os.environ['PYTHONDONTWRITEBYTECODE']
+
+
+def main():
+    VirtualenvRequirements(os.environ).enforce()
+    EnvCreator().create()
+    PySVNHack().install()
+    LocalPackages().install()
+    Inconsistencies().report()
+    PackageSmokeTest().report()
+
+
+class VirtualenvRequirements:
+    def __init__(self, environ):
+        self.environ = environ
+
+    def enforce(self):
+        if self.virtualenv_is_activated():
+            error('refusing to build virtualenv while one is activated')
+        elif self.virtualenv_exists():
+            error('cowardly refusing to overwrite existing environment "%s"'
+                  % ENV_DIR)
+
+    def virtualenv_is_activated(self):
+        return 'VIRTUAL_ENV' in self.environ
+
+    def virtualenv_exists(self):
+        return exists(ENV_DIR)
+
+
+class EnvCreator:
+    def create(self):
+        arguments = ['--ignore-installed',
+                     '--environment=%s' % ENV_DIR,
+                     '--requirement=%s' % REQUIREMENTS_FILE]
+        command = 'pip install %s' % ' '.join(arguments)
+        check_call(command, shell=True)
+
+
+class PySVNHack:
+    def install(self):
+        real_pysvn_path = join(get_python_lib(), 'pysvn')
+        major, minor = sys.version_info[:2]
+        python_version = '%i.%i' % (major, minor)
+        pysvn_symlink_path = ('%s/lib/python%s/site-packages/pysvn' %
+                              (ENV_DIR, python_version))
+        symlink(real_pysvn_path, pysvn_symlink_path)
+
+
+class LocalPackages:
+    def install(self):
+        check_call('. env/bin/activate && bash rebuild.bash', shell=True)
+
+
+class Inconsistencies:
+    def report(self):
+        print
+        print 'Comparing installed packages to requirements:'
+        print
+        bash_command = (
+            'diff -u %s <(pip freeze -r %s)' % (REQUIREMENTS_FILE,
+                                                REQUIREMENTS_FILE))
+        unchecked_call(
+            ". %s/bin/activate && bash -c '%s'" % (ENV_DIR, bash_command),
+            shell=True,
+            stdout=sys.stdout)
+
+
+class PackageSmokeTest:
+    def report(self):
+        print
+        print 'Running import smoke test...'
+        packages_to_import = ['pyforge.model',
+                              'pyforge.app',
+                              'pyforge.controllers.root',
+                              'forgediscussion.model',
+                              'forgegit.model',
+                              'forgehg.model',
+                              'forgesvn.model',
+                              'forgetracker.model',
+                              'forgewiki.model']
+        packages = ', '.join(packages_to_import)
+        check_call(". %s/bin/activate && python -c 'import %s'" %
+                   (ENV_DIR, packages),
+                   shell=True)
+        print 'OK!'
+
+
+def error(message):
+    print 'error: %s' % message
+    sys.exit(1)
+
+
+if __name__ == '__main__':
+    main()
+
---
+++ tests/test_build_virtualenv.py
@@ -0,0 +1,128 @@
+from distutils.sysconfig import get_python_lib
+
+from nose.tools import assert_raises
+from mock import patch
+
+import build_virtualenv
+from build_virtualenv import (VirtualenvRequirements,
+                              error,
+                              EnvCreator,
+                              PySVNHack,
+                              LocalPackages,
+                              Inconsistencies,
+                              PackageSmokeTest)
+
+
+class TestVirtualenvRequirements:
+    @patch('build_virtualenv.error')
+    def test_that_it_doesnt_work_inside_an_activated_env(self, error):
+        environ = dict(VIRTUAL_ENV='/some/path')
+        VirtualenvRequirements(environ).enforce()
+        error.assert_called_with(
+            'refusing to build virtualenv while one is activated')
+
+    @patch('build_virtualenv.error')
+    @patch('build_virtualenv.exists')
+    def test_that_it_doesnt_overwrite_existing_envs(self, exists, error):
+        exists.return_value = True
+        environ = dict()
+        VirtualenvRequirements(environ).enforce()
+        error.assert_called_with(
+            'cowardly refusing to overwrite existing environment "env"')
+
+    @patch('build_virtualenv.error')
+    @patch('build_virtualenv.exists')
+    def test_that_it_allows_env_creation_if_requirements_are_met(self,
+                                                                 exists,
+                                                                 error):
+        exists.return_value = False
+        environ = dict()
+        VirtualenvRequirements(environ).enforce()
+        assert not error.called
+
+
+class TestEnvCreator:
+    @patch('build_virtualenv.check_call')
+    def test_that_it_creates_the_environment(self, check_call):
+        EnvCreator().create()
+        check_call.assert_called_with(
+            'pip install --ignore-installed --environment=env --requirement=requirements.txt',
+            shell=True)
+
+
+class TestPySVNHack:
+    @patch('build_virtualenv.symlink')
+    @patch('build_virtualenv.get_python_lib')
+    def test_that_it_writes_into_the_virtualenv_lib_dir(self,
+                                                        get_python_lib,
+                                                        symlink):
+        fake_lib_dir = '/mylibdir/python2.6/site-packages'
+        get_python_lib.return_value = fake_lib_dir
+        PySVNHack().install()
+        symlink.assert_called_with('/mylibdir/python2.6/site-packages/pysvn',
+                                   'env/lib/python2.6/site-packages/pysvn')
+
+
+class TestLocalPackages:
+    @patch('build_virtualenv.check_call')
+    def test_that_they_run_the_rebuild_script(self, check_call):
+        LocalPackages().install()
+        check_call.assert_called_with(
+            '. env/bin/activate && bash rebuild.bash',
+            shell=True)
+
+
+class TestInconsistencies:
+    @patch('sys.stdout')
+    @patch('build_virtualenv.unchecked_call')
+    def test_that_it_prints_explanation(self, unchecked_call, stdout):
+        Inconsistencies().report()
+        expected = 'Comparing installed packages to requirements:'
+        assert expected in lines_printed_to_stdout(stdout)
+
+    @patch('sys.stdout')
+    @patch('build_virtualenv.unchecked_call')
+    def test_that_it_compares_the_packages(self, unchecked_call, stdout):
+        Inconsistencies().report()
+        unchecked_call.assert_called_with(
+            ". env/bin/activate && bash -c 'diff -u requirements.txt <(pip freeze -r requirements.txt)'",
+            shell=True,
+            stdout=stdout)
+
+
+class TestPackageSmokeTest:
+    @patch('sys.stdout')
+    @patch('build_virtualenv.check_call')
+    def test_that_it_tries_to_import_forge_packages(self,
+                                                    check_call,
+                                                    stdout):
+        PackageSmokeTest().report()
+        check_call.assert_called_with(". env/bin/activate && python -c 'import pyforge.model, pyforge.app, pyforge.controllers.root, forgediscussion.model, forgegit.model, forgehg.model, forgesvn.model, forgetracker.model, forgewiki.model'",
+                                      shell=True)
+
+    @patch('sys.stdout')
+    @patch('build_virtualenv.check_call')
+    def test_that_it_prints_status(self, check_call, stdout):
+        PackageSmokeTest().report()
+        lines_printed = lines_printed_to_stdout(stdout)
+        assert 'Running import smoke test...' in lines_printed
+        assert 'OK!' in lines_printed
+
+
+class TestError:
+    @patch('sys.stdout')
+    @patch('sys.exit')
+    def test_that_it_prints_error_message(self, exit, stdout):
+        error('my message')
+        assert 'error: my message' in lines_printed_to_stdout(stdout)
+
+    @patch('sys.stdout')
+    @patch('sys.exit')
+    def test_that_it_exits(self, exit, stdout):
+        error('my message')
+        exit.assert_called_with(1)
+
+
+def lines_printed_to_stdout(stdout):
+    return [args[0] for args, kwargs in stdout.write.call_args_list]
+
