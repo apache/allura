4e013fc5df9359631cf222e0b78dea2c289e5515
Jenny Steele <jsteele@geek.net>
2010-07-01 13:53:40 -0400

    Fixed a failing test

--- pyforge/pyforge/tests/functional/test_root.py
+++ pyforge/pyforge/tests/functional/test_root.py
@@ -25,7 +25,7 @@ class TestRootController(TestController):
         assert projects[0].find('a').get('href') == '/adobe/adobe-1/'
         assert projects[0].find('img').get('alt') == 'adobe-1 Logo'
         cat_links = response.html.find('div',{'id':'sidebar'}).findAll('li')
-        assert len(cat_links) == 3
+        assert len(cat_links) == 4
         assert cat_links[0].find('a').get('href') == '/browse/clustering'
         assert cat_links[0].find('a').get('class') == 'nav_child'
         assert cat_links[0].find('a').find('span').string == 'Clustering'
