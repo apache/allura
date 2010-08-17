07c81b5172223e34b4a7bf0eb5ec9123cd57844c
Wolf <wolf@geek.net>
2010-08-17 12:23:39 -0400

    update rebuild.bash as per Allura rename (plus automation)

--- rebuild.bash
+++ rebuild.bash
@@ -1,43 +1,7 @@
-echo '# setup allura...'
-pushd allura
-python setup.py develop
-popd
-echo '# setup ForgeDiscussion...'
-pushd ForgeDiscussion
-python setup.py develop
-popd
-echo '# setup ForgeGit...'
-pushd ForgeGit
-python setup.py develop
-popd
-echo '# setup ForgeHg...'
-pushd ForgeHg
-python setup.py develop
-popd
-echo '# setup ForgeLink...'
-pushd ForgeLink
-python setup.py develop
-popd
-echo '# setup ForgeSVN...'
-pushd ForgeSVN
-python setup.py develop
-popd
-echo '# setup ForgeMail...'
-pushd ForgeMail
-python setup.py develop
-popd
-echo '# setup ForgeTracker...'
-pushd ForgeTracker
-python setup.py develop
-popd
-echo '# setup ForgeWiki...'
-pushd ForgeWiki
-python setup.py develop
-popd
-echo '# setup HelloForge...'
-pushd HelloForge
-python setup.py develop
-popd
-pushd ForgeDownloads
-python setup.py develop
-popd
+for APP in Allura *Forge* NoWarnings pyforge
+do
+	echo "# installing $APP dependencies"
+	pushd $APP
+	python setup.py develop
+	popd
+done
