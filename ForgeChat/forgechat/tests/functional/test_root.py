c2745dc1c8d523c75fa06148dae430aa49d7dff7
Rick Copeland <rcopeland@geek.net>
2010-08-23 11:46:42 -0400

    [#814] - add simple test

---
+++ ForgeChat/forgechat/tests/functional/test_root.py
@@ -0,0 +1,7 @@
+from forgechat.tests import TestController
+
+class TestRootController(TestController):
+
+    def test_root_index(self):
+        response = self.app.get('/chat/').follow()
+        assert 'Log for' in response
