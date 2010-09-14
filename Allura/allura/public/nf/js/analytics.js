1fd8d2e43511a0d8855cdbf061ef6496ddb39257
Rick Copeland <rcopeland@geek.net>
2010-09-14 16:26:35 -0400

    [#905] - move more analytics resources to https://

--- Allura/allura/public/nf/js/analytics.js
+++ Allura/allura/public/nf/js/analytics.js
@@ -3,25 +3,29 @@ $(document).ready(function() {
     var holder = $('#analytics');
     // Google
     _gaq.push(['_setAccount', 'UA-32013-6'], ['_trackPageview']);
-    if(document.location.protocol=='https:') {
-        $('<script type="text/javascript" src="https://ssl.google-analytics.com/ga.js"></script>')
-            .appendTo(holder);
-    } else {
-        $('<script type="text/javascript" src="http://www.google-analytics.com/ga.js"></script>')
-            .appendTo(holder);
-    }
     // Collective media
     $('<img src="//b.collective-media.net/seg/cm/cm_aa_gn1" width="1" height="1" alt=""/>')
         .appendTo(holder);
     // Quantcast
     _qoptions={qacct:"p-93nPV3-Eb4x22"};
-    $('<script type="text/javascript" src="http://secure.quantserve.com/quant.js"></script>')
-        .appendTo(holder);
-    // comScore
     if(document.location.protocol=='https:') {
+        // Google
+        $('<script type="text/javascript" src="https://ssl.google-analytics.com/ga.js"></script>')
+            .appendTo(holder);
+        // Quantcast
+        $('<script type="text/javascript" src="https://secure.quantserve.com/quant.js"></script>')
+            .appendTo(holder);
+        // comScore
         $('<script type="text/javascript" src="https://sb.scorecardresearch.com/beacon.js"></script>')
             .appendTo(holder);
     } else {
+        // Google
+        $('<script type="text/javascript" src="http://www.google-analytics.com/ga.js"></script>')
+            .appendTo(holder);
+        // Quantcast
+        $('<script type="text/javascript" src="https://secure.quantserve.com/quant.js"></script>')
+            .appendTo(holder);
+        // comScore
         $('<script type="text/javascript" src="http://b.scorecardresearch.com/beacon.js"></script>')
             .appendTo(holder);
     }
