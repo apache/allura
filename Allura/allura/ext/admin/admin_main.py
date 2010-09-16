9194e98fb8f289d29f39e9358b5ffdb7cb9c6b5f
Paul Sokolovsky <psokolovsky@corp.sourceforge.com>
2010-09-16 13:16:30 +0300

    [#906] Convert Project Admin - Tools page to Jinja.

--- Allura/allura/ext/admin/admin_main.py
+++ Allura/allura/ext/admin/admin_main.py
@@ -166,7 +166,7 @@ class ProjectAdminController(BaseController):
         return dict()
 
     @without_trailing_slash
-    @expose('allura.ext.admin.templates.project_tools')
+    @expose('jinja:project_tools.html')
     def tools(self, **kw):
         c.markdown_editor = W.markdown_editor
         c.label_edit = W.label_edit
