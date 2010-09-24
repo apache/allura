a4fcd5543cafc474c59f03b6be25cd2726ea49ef
Paul Sokolovsky <psokolovsky@corp.sourceforge.com>
2010-09-24 22:10:49 +0300

    [#930] Convert oembed templates to Jinja.

--- Allura/allura/controllers/oembed.py
+++ Allura/allura/controllers/oembed.py
@@ -20,7 +20,7 @@ log = logging.getLogger(__name__)
 class OEmbedController(BaseController):
     '''Controller that serves up oembedded resources'''
 
-    @expose('allura.templates.oembed.generic')
+    @expose('jinja:oembed/generic.html')
     def index(self, href, **kw):
         try:
             response = g.oembed_consumer.embed(href)
@@ -29,18 +29,18 @@ class OEmbedController(BaseController):
         data = response.getData()
         log.info('Got response:\n%s', pformat(data))
         if isinstance(response, oembed.OEmbedPhotoResponse):
-            override_template(self.index, 'genshi:allura.templates.oembed.photo')
+            override_template(self.index, 'jinja:oembed/photo.html')
         elif isinstance(response, oembed.OEmbedVideoResponse):
-            override_template(self.index, 'genshi:allura.templates.oembed.html_tpl')
+            override_template(self.index, 'jinja:oembed/html_tpl.html')
         elif isinstance(response, oembed.OEmbedRichResponse):
-            override_template(self.index, 'genshi:allura.templates.oembed.html_tpl')
+            override_template(self.index, 'jinja:oembed/html_tpl.html')
         elif isinstance(response, oembed.OEmbedLinkResponse):
             if data['provider_name'] == 'Twitter Status':
-                override_template(self.index, 'genshi:allura.templates.oembed.link_twitter')
+                override_template(self.index, 'jinja:oembed/link_twitter.html')
             elif data['provider_name'] == 'My Opera Community':
-                override_template(self.index, 'genshi:allura.templates.oembed.link_opera')
+                override_template(self.index, 'jinja:oembed/link_opera.html')
             else: # pragma no cover
-                override_template(self.index, 'genshi:allura.templates.oembed.link')
+                override_template(self.index, 'jinja:oembed/link.html')
         else:
                 pass
         return dict(href=href, data=data)
