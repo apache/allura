d5dad23f333e538e6707ef6961f9c196801dfbc2
Jenny Steele <jsteele@geek.net>
2010-10-12 15:34:29 -0400

    [#1034] Bulk edit UI tweaks

--- ForgeTracker/forgetracker/widgets/resources/js/mass-edit.js
+++ ForgeTracker/forgetracker/widgets/resources/js/mass-edit.js
@@ -1,5 +1,5 @@
 function update_tickets(){
-    var $checked=$('input:checked'), count=$checked.length;
+    var $checked=$('tbody.ticket-list input:checked'), count=$checked.length;
 
     if ( !count ) {
         $('#result').text('No tickets selected for update.');
@@ -21,11 +21,11 @@ function update_tickets(){
 $(function(){
     $('#assigned_to').val('');
     $('#select_all').click(function(){
-        $('tbody.ticket-list input[type=checkbox]').attr('checked', 'checked');
-        return false;
-    });
-    $('#select_none').click(function(){
-        $('tbody.ticket-list input[type=checkbox]').removeAttr('checked');
-        return false;
+        if(this.checked){
+            $('tbody.ticket-list input[type=checkbox]').attr('checked', 'checked');
+        }
+        else{
+            $('tbody.ticket-list input[type=checkbox]').removeAttr('checked');
+        }
     });
 });
