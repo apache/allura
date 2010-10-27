877771b0236e06273e31555badec2932f2bdddb6
Paul Sokolovsky <psokolovsky@corp.sourceforge.com>
2010-10-27 16:19:03 +0300

    [#1029] Add optparse, refactor to use iterator protocol.

--- migrate-3rdparty/trac_export.py
+++ migrate-3rdparty/trac_export.py
@@ -1,33 +1,55 @@
 from pprint import pprint
 import csv
 import urllib2
-from cStringIO import StringIO
 import json
 import time
+from optparse import OptionParser
+from itertools import islice
 
 import feedparser
 from html2text import html2text
 
-from allura.lib import rest_api
+
+def parse_options():
+    optparser = OptionParser(usage=''' %prog --base-url=
+ 
+Export ticket data from a Trac instance''')
+    optparser.add_option('-u', '--base-url', dest='base_url', default='https://sourceforge.net/apps/trac/sourceforge', help='Base Trac URL (%default)')
+    optparser.add_option('--start', dest='start_id', type='int', default=1, help='Start with given ticket numer (or next accessible)')
+    optparser.add_option('--limit', dest='limit', type='int', default=None, help='Limit number of tickets')
+    options, args = optparser.parse_args()
+    if len(args) != 0:
+        optparser.error("Wrong number of arguments.")
+    if not options.base_url:
+        optparser.error("URL are required.")
+    return options, args
 
 
 class TracExport(object):
 
+    PAGE_SIZE = 100
     TICKET_URL = '/ticket/%d'
     QUERY_MAX_ID_URL  = '/query?col=id&order=id&desc=1&max=2'
-    QUERY_BY_PAGE_URL = '/query?col=id&col=time&col=changetime&order=id&max=100&page=%d'
+    QUERY_BY_PAGE_URL = '/query?col=id&col=time&col=changetime&order=id&max=' + str(PAGE_SIZE)+ '&page=%d'
 
     FIELD_MAP = {
         'reporter': 'submitter',
         'owner': 'assigned_to',
     }
 
-    def __init__(self, base_url):
+    def __init__(self, base_url, start_id=1):
+        """start_id - start with at least that ticket number (actual returned
+                      ticket may have higher id if we don't have access to exact 
+                      one).
+        """
         self.base_url = base_url
         # Contains additional info for a ticket which cannot
         # be get with single-ticket export (create/mod times is
         # and example).
         self.ticket_map = {}
+        self.ticket_queue = []
+        self.start_id = start_id
+        self.page = (start_id - 1) / self.PAGE_SIZE + 1
 
     def remap_fields(self, dict):
         "Remap fields to adhere to standard taxonomy."
@@ -42,6 +64,12 @@ class TracExport(object):
     def full_url(self, suburl, type):
         glue = '&' if '?' in suburl else '?'
         return self.base_url + suburl + glue + 'format=' + type
+
+    @staticmethod
+    def trac2z_date(s):
+        assert len(s) == 25
+        assert s.endswith('+00:00')
+        return s[0:10] + 'T' + s[11:19] + 'Z'
 
     def csvopen(self, url):
         print url
@@ -76,38 +104,6 @@ class TracExport(object):
             res.append(c)
         return res
 
-    def get_ticket(self, id):
-        t = self.parse_ticket_body(id)
-        t['comments'] = self.parse_ticket_comments(id)
-        if id in self.ticket_map:
-            t.update(self.ticket_map[id])
-        return t
-
-    def get_ticket_ids_csv(self):
-        url = self.full_url(self.QUERY_URL, 'csv')
-        print url
-        f = urllib2.urlopen(url, timeout=None)
-        reader = csv.reader(f)
-        cols = reader.next()
-        ids = [r for r in reader]
-        return ids
-
-    def get_ticket_ids(self):
-        # As Trac has only one tracker, and number ticket sequantally, 
-        # We have two choices here:
-        # 1. Get the last existing ticket id, and just make sequence
-        #    from 1 to that id. But then we should be ready to teh fact
-        #    that some tickets in this range will be unavailable.
-        # 2. Export artifact list and get ids which are really accessible
-        #    to the current user.
-        # It turns out that we need to paginate artifact list, so it's
-        # some time and extra traffic, so first method used by default.
-        if False:
-            max = self.get_max_ticket_id()
-            return xrange(1, max + 1)
-        else:
-            return self.enumerate_ticket_ids()
-
     def get_max_ticket_id(self):
         url = self.full_url(self.QUERY_MAX_ID_URL, 'csv')
         f = self.csvopen(url)
@@ -115,41 +111,52 @@ class TracExport(object):
         fields = reader.next()
         print fields
         return int(fields['id'])
-        
-    @staticmethod
-    def trac2z_date(s):
-        assert len(s) == 25
-        assert s.endswith('+00:00')
-        return s[0:10] + 'T' + s[11:19] + 'Z'
 
-    def enumerate_ticket_ids(self, page=1, limit=-1):
+    def get_ticket(self, id, extra={}):
+        t = self.parse_ticket_body(id)
+        t['comments'] = self.parse_ticket_comments(id)
+        t.update(extra)
+        return t
+
+    def next_ticket_ids(self):
         'Go thru ticket list and collect available ticket ids.'
         # We could just do CSV export, which by default dumps entire list
         # Alas, for many busy servers with long ticket list, it will just 
         # time out. So, let's paginate it instead.
         res = []
-        while limit != 0:
-            url = self.full_url(self.QUERY_BY_PAGE_URL % page, 'csv')
-            try:
-                f = self.csvopen(url)
-            except urllib2.HTTPError, e:
-                if 'emulated' in e.msg:
-                    body = e.fp.read()
-                    if 'beyond the number of pages in the query' in body:
-                        break
-                raise
-            reader = csv.reader(f)
-            cols = reader.next()
-            for r in reader:
-                if r and r[0].isdigit():
-                    id = int(r[0])
-                    self.ticket_map[id] = {'date': self.trac2z_date(r[1]), 'date_updated': self.trac2z_date(r[2])}
-                    res.append(id)
-            page += 1
-            if limit > 0:
-                limit -= 1
+
+        url = self.full_url(self.QUERY_BY_PAGE_URL % self.page, 'csv')
+        try:
+            f = self.csvopen(url)
+        except urllib2.HTTPError, e:
+            if 'emulated' in e.msg:
+                body = e.fp.read()
+                if 'beyond the number of pages in the query' in body:
+                    raise StopIteration
+            raise
+        reader = csv.reader(f)
+        cols = reader.next()
+        for r in reader:
+            if r and r[0].isdigit():
+                id = int(r[0])
+                extra = {'date': self.trac2z_date(r[1]), 'date_updated': self.trac2z_date(r[2])}
+                res.append((id, extra))
+        self.page += 1
 
         return res
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        while True:
+            if not self.ticket_queue:
+                self.ticket_queue = self.next_ticket_ids()
+            id, extra = self.ticket_queue.pop(0)
+            if id >= self.start_id:
+                break
+        return self.get_ticket(id, extra)
+            
         
 class DateJSONEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -158,20 +165,7 @@ class DateJSONEncoder(json.JSONEncoder):
         return json.JSONEncoder.default(self, obj)
 
 if __name__ == '__main__':
-    TRAC_BASE_URL = 'http://sourceforge.net/apps/trac/sourceforge'
-    ex = TracExport(TRAC_BASE_URL)
-#    d = ex.parse_ticket_body(9)
-#    pprint(d)
-#    d = ex.parse_ticket_comments(9)
-#    pprint(d)
-#    d = ex.get_ticket(9)
-#    pprint(d)
-#    d = ex.get_max_ticket_id()
-#    d = ex.get_ticket_ids()
-    d = ex.enumerate_ticket_ids(limit=1)
-    ids = [3]
-    doc = [ex.get_ticket(i) for i in ids]
+    options, args = parse_options()
+    ex = TracExport(options.base_url, start_id=options.start_id)
+    doc = [t for t in islice(ex, options.limit)]
     print json.dumps(doc, cls=DateJSONEncoder, indent=2)
-#    print d
-#    print ex.ticket_map
-
