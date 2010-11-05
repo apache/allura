8e6fed2eaba06c7130a554d7f1e0482bdf0df447
Rick Copeland <rcopeland@geek.net>
2010-11-05 17:04:28 -0400

    [#945] Update git fetch instructions

--- ForgeGit/forgegit/model/git_repo.py
+++ ForgeGit/forgegit/model/git_repo.py
@@ -38,9 +38,10 @@ class Repository(M.Repository):
 
     def merge_command(self, merge_request):
         '''Return the command to merge a given commit to a given target branch'''
-        return 'git checkout %s;\ngit pull git://%s %s' % (
+        return 'git checkout %s;\ngit fetch git://%s %s;\ngit merge %s' % (
             merge_request.target_branch,
             merge_request.downstream_repo_url,
+            merge_request.downstream.commit_id,
             merge_request.downstream.commit_id)
 
 class GitImplementation(M.RepositoryImplementation):
