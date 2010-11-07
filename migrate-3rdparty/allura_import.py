b9cce22a7b677ca6cf7d6afd644c99bf38568c02
Paul Sokolovsky <psokolovsky@corp.sourceforge.com>
2010-11-07 16:44:18 +0200

    [#1029] Allow to specify project/tracker from command line.

--- migrate-3rdparty/allura_import.py
+++ migrate-3rdparty/allura_import.py
@@ -10,19 +10,23 @@ from datetime import datetime
 
 
 def parse_options():
-    optparser = OptionParser(usage=''' %prog --api-key= --secret-key= <JSON dump>
+    optparser = OptionParser(usage='''%prog [options] <JSON dump>
 
-Import project data dump in JSON format into Allura project.''')
+Import project data dump in JSON format into an Allura project.''')
     optparser.add_option('-a', '--api-key', dest='api_key', help='API key')
     optparser.add_option('-s', '--secret-key', dest='secret_key', help='Secret key')
+    optparser.add_option('-p', '--project', dest='project', help='Project to import to')
+    optparser.add_option('-t', '--tracker', dest='tracker', help='Tracker to import to')
     optparser.add_option('-u', '--base-url', dest='base_url', default='https://sourceforge.net', help='Base Allura URL (%default)')
     optparser.add_option('--validate', dest='validate', action='store_true', help='Validate import data')
     optparser.add_option('-v', '--verbose', dest='verbose', action='store_true', help='Verbose operation')
     options, args = optparser.parse_args()
     if len(args) != 1:
-        optparser.error("Wrong number of arguments.")
+        optparser.error("Wrong number of arguments")
     if not options.api_key or not options.secret_key:
-        optparser.error("Keys are required.")
+        optparser.error("Keys are required")
+    if not options.project or not options.tracker:
+        optparser.error("Target project and tracker are required")
     return options, args
 
 
@@ -57,10 +61,11 @@ class AlluraRestClient(object):
     
 if __name__ == '__main__':
     options, args = parse_options()
+    url = '/rest/p/' + options.project + '/' + options.tracker
     if options.validate:
-        url = '/rest/p/test/bugs/validate_import'
+        url += '/validate_import'
     else:
-        url = '/rest/p/test/bugs/perform_import'
+        url += '/perform_import'
 
     cli = AlluraRestClient(options.base_url, options.api_key, options.secret_key)
     print cli.call(url, doc=open(args[0]).read())
