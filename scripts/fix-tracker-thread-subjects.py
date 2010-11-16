5481e634acec235502ca79cfc72a359c113a2f61
Wolf <wolf@geek.net>
2010-11-16 16:36:03 -0500

    [#1181] script to clear out ticket thread subjects
    Earlier, we improved the subject line for ticket related emails
    (e0dbb34).  Before that fix, a ticket's thread used a one-time generated
    message of the form '#yyy discussion'.  That subject was stored in the
    Thread itself and used as the subject on all emails sent by the ticket.
    By promoting subject generation up to the ticket, we could include the
    ticket summary, giving, e.g., '#yyy Some problem needs fixing'.  A much
    more helpful email subject.
    
    All tickets created after e0dbb34 went live now have no subject stored
    in the Thread, and so use Ticket.email_subject to get the helpful
    subject.
    
    Unfortunately, existing tickets _don't_ get this behavior, because their
    Threads already have a (bad) subject.  When the Thread has a good
    subject it will be more specific than the underlying artifact, and so we
    want it to 'win'.  We don't want any of the existing ticket Thread
    subjects to win, so we need a script to clear them all out.  That's what
    this script does.

---
+++ scripts/fix-tracker-thread-subjects.py
@@ -0,0 +1,33 @@
+import sys
+import logging
+
+from pylons import c
+
+from ming.orm import session
+
+from allura import model as M
+from forgetracker import model as TM
+
+log = logging.getLogger(__name__)
+
+def main():
+    test = sys.argv[-1] == 'test'
+    all_projects = M.Project.query.find().all()
+    log.info('Fixing tracker thread subjects')
+    for project in all_projects:
+        if project.parent_id: continue
+        c.project = project
+        all_tickets = TM.Ticket.query.find() # will find all tickets for all trackers in this project
+        if not all_tickets.count(): continue
+        for ticket in all_tickets:
+            thread = ticket.get_discussion_thread()
+            thread.subject = ''
+        if test:
+            log.info('... would fix ticket threads in %s', p.shortname)
+        else:
+            log.info('... fixing ticket threads in %s', p.shortname)
+            session(project).flush()
+        session(project).clear()
+
+if __name__ == '__main__':
+    main()
