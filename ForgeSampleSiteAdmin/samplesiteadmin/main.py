22b3d9947d7c004aa72ef571b2f19904081bf6f2
Paul Sokolovsky <psokolovsky@corp.sourceforge.com>
2010-11-18 01:38:40 +0200

    [#1128] Add sample tool to generate special API keys.

---
+++ ForgeSampleSiteAdmin/samplesiteadmin/main.py
@@ -0,0 +1,98 @@
+import os
+import difflib
+import logging
+from pprint import pformat
+from datetime import datetime, timedelta
+
+from pylons import c, g
+import pkg_resources
+from pylons import c, request
+from tg import expose, redirect, flash
+from webob import exc
+from pymongo.bson import ObjectId
+
+
+from ming.orm import session
+from allura import version
+from allura.app import Application, WidgetController, ConfigOption, SitemapEntry
+from allura.lib import helpers as h
+from allura.ext.project_home import model as M
+from allura.lib.security import require, has_project_access, has_artifact_access
+from allura.model import User, ArtifactLink
+from allura.model import ApiToken
+from allura.controllers import BaseController
+
+log = logging.getLogger(__name__)
+
+
+class SampleSiteAdminApp(Application):
+    __version__ = version.__version__
+    installable = True
+    tool_label='Sample Site Admin'
+    default_mount_label='SiteAdmin'
+    default_mount_point='site'
+
+    def __init__(self, user, config):
+        Application.__init__(self, user, config)
+        self.root = SiteAdminController()
+
+    def admin_menu(self):
+        return []
+
+    def install(self, project):
+        pr = c.user.project_role()
+        if pr:
+            for perm in self.permissions:
+                self.config.acl[perm] = [ pr._id ]
+
+    def uninstall(self, project): # pragma no cover
+        raise NotImplementedError, "uninstall"
+
+class SiteAdminController(BaseController):
+
+    def _check_security(self):
+        require(has_project_access('read'),
+                'Read access required')
+
+    @expose('jinja:user_special_api_keys.html')
+    def api_keys(self, **data):
+        import json
+        import dateutil.parser
+        if request.method == 'POST':
+            log.info('api_keys: %s', data)
+            ok = True
+            for_user = User.by_username(data['for_user'])
+            if not for_user:
+                ok = False
+                flash('User not found')
+            caps = None
+            try:
+                caps = json.loads(data['caps'])
+            except ValueError:
+                ok = False
+                flash('JSON format error')
+            if type(caps) is not type({}):
+                ok = False
+                flash('Capabilities must be a dictionary, mapping capability name to optional discriminator(s) (or "")')
+            try:
+                expires = dateutil.parser.parse(data['expires'])
+            except ValueError:
+                ok = False
+                flash('Date format error')
+                
+            if ok:
+                tok = None
+                try:
+                    tok = ApiToken(user_id=for_user._id, capabilities=caps, expires=expires)
+                    session(tok).flush()
+                    log.info('New token: %s', tok)
+                except:
+                    log.exception('Could not create API key:')
+                    flash('Error creating API key')
+        else:
+            data = {'expires': datetime.now() + timedelta(days=1)}
+
+        username = c.project.shortname.split('/')[1]
+        data['user'] = User.by_username(username)
+        data['token_list'] = ApiToken.query.find(dict(expires={'$ne': None})).all()
+        return data
