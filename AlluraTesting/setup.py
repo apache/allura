bd831622eb8dfb6b4621ce8ea13f15c0dd9b0686
Paul Sokolovsky <psokolovsky@corp.sourceforge.com>
2010-12-15 22:26:10 +0200

    [#1233] Add new package, alluratest, for test support code.

---
+++ AlluraTesting/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+import sys, os
+
+setup(name='AlluraTesting',
+      version='0.1',
+      description="Allura testing support",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='',
+      author_email='',
+      url='',
+      license='',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ]
+      )
