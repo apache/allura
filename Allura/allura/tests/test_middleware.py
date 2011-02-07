b875af2ed5b60e4c441ae3d1fd0e9c3505b2fa0c
Paul Sokolovsky <psokolovsky@geek.net>
2011-02-07 17:22:15 +0200

    [#1273] Simplify test, based on @patch caveats.
    Signed-off-by: Paul Sokolovsky <psokolovsky@geek.net>

--- Allura/allura/tests/test_middleware.py
+++ Allura/allura/tests/test_middleware.py
@@ -20,34 +20,13 @@ def test_make_app():
     conf = appconfig('config:%s#main' % get_config_file(), relative_to=config.here)
     make_app(conf.global_conf, **conf.local_conf)
 
-#@patch('sf.phpsession.SFXSessionMgr')
-@patch('sqlalchemy.create_engine')
+@patch('sf.phpsession.SFXSessionMgr.__new__')
 @patch('sfx.middleware.configure_databases')
-def test_all_middleware(mk2, session_mgr):
-    print session_mgr
-    #, sf.phpsession.SFXSessionMgr
-    from sf.phpsession import SFXSessionMgr; print SFXSessionMgr
-
+def test_all_middleware(__new__, configure_databases):
     conf = appconfig('config:%s#main' % get_config_file(), relative_to=config.here)
     conf.local_conf['auth.method'] = 'sfx'
     conf.local_conf['stats.sample_rate'] = '0.25'
-    conf.local_conf['sfx.sys_session_db_username'] = 'foo'
-    conf.local_conf['sfx.sys_session_db_password'] = 'passwd'
-    conf.local_conf['sfx.sys_session_db_host'] = 'localhost'
-    conf.local_conf['sfx.sys_session_db_database'] = 'db'
-    conf.local_conf['sfx.sys_session_db_pool_recycle'] = 1
-    conf.local_conf['sfx.sys_session_db_pool_size'] = 1
-    conf.local_conf['sfx.sys_session_db_pool_max_overflow'] = 1
-    conf.local_conf['sfx.scheme'] = 'dontcare'
     app = make_app(conf.global_conf, **conf.local_conf)
-    d = defaultdict(lambda: "str")
-    d['wsgi.multiprocess'] = False
-    d['paste.evalexception'] = False
-    from cStringIO import StringIO
-    d['wsgi.errors'] = StringIO()
-    env = Environ()
-    env.set_environment(d)
-    app(d, lambda *a, **kw: None)
 
 def test_environ():
     d = {'foo': 'qwe', 'bar': 1234}
