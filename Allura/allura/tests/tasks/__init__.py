d8f52a0f99d5e9b9890b14227c1ea626635a248c
Rick Copeland <rcopeland@geek.net>
2011-03-07 16:42:55 -0500

    [#1601] Remove dead code that depended on lamson
    Signed-off-by: Rick Copeland <rcopeland@geek.net>

--- Allura/allura/tests/tasks/__init__.py
+++
@@ -1,2 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Tasks test suite for the tasks of the application."""
--- ForgeMail/forgemail/app/handlers/index.py
+++
@@ -1,9 +0,0 @@
-from lamson import queue
-from lamson.routing import route, stateless
-import logging
-
-@route("(address)@(host)")
-@stateless
-def START(message, post_name=None, host=None):
-    logging.debug("Got message from %s", message['from'])
-
