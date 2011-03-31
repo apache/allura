d84715952d777ab537828f0e36fe6519f95b94b7
Rick Copeland <rcopeland@geek.net>
2011-03-31 09:48:44 -0400

    [#1840] if there is a single exception in an event, raise that exception rather than the COmpoundError
    Signed-off-by: Rick Copeland <rcopeland@geek.net>

--- Allura/allura/tasks/event_tasks.py
+++ Allura/allura/tasks/event_tasks.py
@@ -12,5 +12,8 @@ def event(event_type, *args, **kwargs):
         except:
             exceptions.append(sys.exc_info())
     if exceptions:
-        raise CompoundError(*exceptions)
+        if len(exceptions) == 1:
+            raise exceptions[0][0], exceptions[0][1], exceptions[0][2]
+        else:
+            raise CompoundError(*exceptions)
 
