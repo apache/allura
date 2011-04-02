765bc15369e68f448f5ec9e4f84e7a79a2c914b0
Paul Sokolovsky <psokolovsky@geek.net>
2011-04-03 02:08:57 +0300

    [#1767] AlluraApiClient: Add auto-retry on network errors.
    Signed-off-by: Paul Sokolovsky <psokolovsky@geek.net>

--- scripts/allura_api.py
+++ scripts/allura_api.py
@@ -10,11 +10,12 @@ from datetime import datetime
 
 class AlluraApiClient(object):
 
-    def __init__(self, base_url, api_key, secret_key, verbose=False):
+    def __init__(self, base_url, api_key, secret_key, verbose=False, retry=True):
         self.base_url = base_url
         self.api_key = api_key
         self.secret_key = secret_key
         self.verbose = verbose
+        self.retry = retry
 
     def sign(self, path, params):
         params.append(('api_key', self.api_key))
@@ -28,12 +29,17 @@ class AlluraApiClient(object):
         url = urlparse.urljoin(self.base_url, url)
         params = self.sign(urlparse.urlparse(url).path, params.items())
 
-        try:
-            result = urllib2.urlopen(url, urllib.urlencode(params))
-            resp = result.read()
-            return json.loads(resp)
-        except urllib2.HTTPError, e:
-            if self.verbose:
-                error_content = e.read()
-                e.msg += '. Error response:\n' + error_content
-            raise e
+        while True:
+            try:
+                result = urllib2.urlopen(url, urllib.urlencode(params))
+                resp = result.read()
+                return json.loads(resp)
+            except urllib2.HTTPError, e:
+                if self.verbose:
+                    error_content = e.read()
+                    e.msg += '. Error response:\n' + error_content
+                raise e
+            except (urllib2.URLError, IOError):
+                if self.retry:
+                    continue
+                raise
