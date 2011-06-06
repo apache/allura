1af75016586f0a8571ce430237eac935160be5bd
Jenny Steele <jsteele@geek.net>
2011-06-06 18:04:23 -0400

    [#2194] Tweak to allow markdown_to_html to show wiki-only macros and select project in correct neighborhood
    Signed-off-by: Jenny Steele <jsteele@geek.net>

--- Allura/allura/controllers/static.py
+++ Allura/allura/controllers/static.py
@@ -13,13 +13,16 @@ class NewForgeController(object):
 
     @expose()
     @without_trailing_slash
-    def markdown_to_html(self, markdown, project=None, app=None):
+    def markdown_to_html(self, markdown, neighborhood=None, project=None, app=None):
         """Convert markdown to html."""
         if project:
+            if neighborhood:
+                n = M.Neighborhood.query.get(name=neighborhood)
+                project = M.Project.query.get(shortname=project, neighborhood_id=n._id)
             g.set_project(project)
             if app:
                 g.set_app(app)
-        html = g.markdown.convert(markdown)
+        html = g.markdown_wiki.convert(markdown)
         return html
 
     @expose()
