2bd5b9bdbf40bf86334730df655152856f75ef6a
Wolf <wolf@geek.net>
2011-06-13 15:25:16 -0400

    [#2257] script to fix duplicate ordinals in project mounts
    Signed-off-by: Wolf <wolf@geek.net>

---
+++ scripts/update-ordinals.py
@@ -0,0 +1,66 @@
+import sys
+import logging
+
+from pylons import c
+from ming.orm import session
+from ming.orm.ormsession import ThreadLocalORMSession
+
+from allura import model as M
+
+log = logging.getLogger(__name__)
+handler = logging.StreamHandler(sys.stdout)
+log.addHandler(handler)
+
+def main():
+    test = sys.argv[-1] == 'test'
+    num_projects_examined = 0
+    log.info('Examining all projects for mount order.')
+    for some_projects in chunked_project_iterator({}):
+        for project in some_projects:
+            c.project = project
+            mounts = project.ordered_mounts(include_search=True)
+
+            # ordered_mounts() means duplicate ordinals (if any) will be next to each other
+            duplicates_found = False
+            prev_ordinal = None
+            for mount in mounts:
+                if mount['ordinal'] == prev_ordinal:
+                    duplicates_found = True
+                    break
+                prev_ordinal = mount['ordinal']
+
+            if duplicates_found:
+                if test:
+                    log.info('Would renumber mounts for project "%s".' % project.shortname)
+                else:
+                    log.info('Renumbering mounts for project "%s".' % project.shortname)
+                    for i, mount in enumerate(mounts):
+                        if 'ac' in mount:
+                            mount['ac'].options['ordinal'] = i
+                        elif 'sub' in mount:
+                            mount['sub'].ordinal = i
+                    ThreadLocalORMSession.flush_all()
+
+            num_projects_examined += 1
+            session(project).clear()
+
+        log.info('%s projects examined.' % num_projects_examined)
+
+
+PAGESIZE=1024
+
+def chunked_project_iterator(q_project):
+    '''shamelessly copied from refresh-all-repos.py'''
+    page = 0
+    while True:
+        results = (M.Project.query
+                   .find(q_project)
+                   .skip(PAGESIZE*page)
+                   .limit(PAGESIZE)
+                   .all())
+        if not results: break
+        yield results
+        page += 1
+
+if __name__ == '__main__':
+    main()
