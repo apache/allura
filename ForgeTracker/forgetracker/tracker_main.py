apache/allura

commit 9dafc47f50d8aea8489243669e657e394a6d9a9e

[#2368] Fixed delay in Milestone counts, reviewed cache invalidation code
Signed-off-by: Wayne Witzel III <wwitzel@geek.net>

ForgeTracker/forgetracker/tracker_main.py
@@ -155,8 +155,7 @@ class ForgeTrackerApp(Application):
                         h.text.truncate(m.name, 72),
                         self.url + fld.name[1:] + '/' + m.name + '/',
                         className='nav_child',
-                        small=c.app.globals.milestone_count(
-                            '%s:%s' % (fld.name, m.name))['hits']))
+                        small=TM.Ticket.query.find({"custom_fields._milestone": m.name}).count()))
         if ticket.isdigit():
             ticket = TM.Ticket.query.find(dict(app_config_id=self.config._id,ticket_num=int(ticket))).first()
         else:

ForgeTracker/forgetracker/tests/functional/test_root.py
@@ -48,6 +48,11 @@ class TestFunctionalController(TestController):
         assert 'Milestone' in ticket_view
         assert '1.0' in ticket_view
 
+    def test_milestone_count(self):
+        self.new_ticket(summary='test new with milestone', **{'_milestone':'1.0'})
+        ticket_view = self.new_ticket(summary='test new with milestone', **{'_milestone':'1.0'}).follow()
+        assert '<small>2</small>' in ticket_view
+        
     def test_new_ticket_form(self):
         response = self.app.get('/bugs/new/')
         form = response.forms[1]

commit f831d6d3a0f8b3b59de43f6366c0bcfffa2ad8eb

[#2582] use app_config_id when querying milestone values
Change from 9dafc47f introduced a bug where milestones were queried
across all tool instances, not just the current one.

Signed-off-by: Dave Brondsema <dbrondsema@geek.net>

ForgeTracker/forgetracker/tracker_main.py
@@ -155,7 +155,7 @@ class ForgeTrackerApp(Application):
                         h.text.truncate(m.name, 72),
                         self.url + fld.name[1:] + '/' + m.name + '/',
                         className='nav_child',
-                        small=TM.Ticket.query.find({"custom_fields._milestone": m.name}).count()))
+                        small=TM.Ticket.query.find({"custom_fields._milestone": m.name, "app_config_id": c.app.config._id}).count()))
         if ticket.isdigit():
             ticket = TM.Ticket.query.find(dict(app_config_id=self.config._id,ticket_num=int(ticket))).first()
         else:

ForgeTracker/forgetracker/tests/functional/test_root.py
@@ -52,7 +52,7 @@ class TestFunctionalController(TestController):
         self.new_ticket(summary='test new with milestone', **{'_milestone':'1.0'})
         ticket_view = self.new_ticket(summary='test new with milestone', **{'_milestone':'1.0'}).follow()
         assert '<small>2</small>' in ticket_view
-        
+
     def test_new_ticket_form(self):
         response = self.app.get('/bugs/new/')
         form = response.forms[1]
@@ -95,14 +95,16 @@ class TestFunctionalController(TestController):
 
     def test_two_trackers(self):
         summary = 'test two trackers'
-        ticket_view = self.new_ticket('/doc-bugs/', summary=summary).follow()
+        ticket_view = self.new_ticket('/doc-bugs/', summary=summary, _milestone='1.0').follow()
         ThreadLocalORMSession.flush_all()
         M.MonQTask.run_ready()
         ThreadLocalORMSession.flush_all()
         assert_true(summary in ticket_view)
         index_view = self.app.get('/doc-bugs/')
         assert_true(summary in index_view)
+        assert_true(sidebar_contains(index_view, '<span class="has_small">1.0</span><small>1</small>'))
         index_view = self.app.get('/bugs/')
+        assert_false(sidebar_contains(index_view, '<span class="has_small">1.0</span><small>1</small>'))
         assert_false(summary in index_view)
 
     def test_render_ticket(self):