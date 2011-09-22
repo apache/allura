74e2a9c05e391b0af562767f2fd9afb2cdb5efb3
Jenny Steele <jsteele@geek.net>
2011-09-22 07:37:27 -0700

    [#2836] Add new audience categories to allura trove
    Signed-off-by: Jenny Steele <jsteele@geek.net>

--- scripts/migrations/016-add-trove-category-lppl.py
+++ scripts/migrations/016-add-trove-category-lppl.py
@@ -40,6 +40,31 @@ def main():
                     shortname="ios",
                     fullname="Apple iPhone",
                     fullpath="Operating System :: Handheld/Embedded Operating Systems :: Apple iPhone")
+    M.TroveCategory(trove_cat_id=863,
+                    trove_parent_id=534,
+                    shortname="architects",
+                    fullname="Architects",
+                    fullpath="Intended Audience :: by End-User Class :: Architects")
+    M.TroveCategory(trove_cat_id=864,
+                    trove_parent_id=534,
+                    shortname="auditors",
+                    fullname="Auditors",
+                    fullpath="Intended Audience :: by End-User Class :: Auditors")
+    M.TroveCategory(trove_cat_id=865,
+                    trove_parent_id=534,
+                    shortname="testers",
+                    fullname="Testers",
+                    fullpath="Intended Audience :: by End-User Class :: Testers")
+    M.TroveCategory(trove_cat_id=866,
+                    trove_parent_id=534,
+                    shortname="secpros",
+                    fullname="Security Professionals",
+                    fullpath="Intended Audience :: by End-User Class :: Security Professionals")
+    M.TroveCategory(trove_cat_id=867,
+                    trove_parent_id=535,
+                    shortname="secindustry",
+                    fullname="Security",
+                    fullpath="Intended Audience :: by Industry or Sector :: Security")
     ThreadLocalORMSession.flush_all()
     ThreadLocalORMSession.close_all()
 
