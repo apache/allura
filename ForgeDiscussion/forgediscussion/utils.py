3def92747c2a1be6d54c9a949ccc5126abeaed24
Dave Brondsema <dbrondsema@geek.net>
2011-10-28 02:34:40 +0000

    [#2962] [#3050] store anon_posts field on the Forum; cleanup
    Signed-off-by: Dave Brondsema <dbrondsema@geek.net>

--- ForgeDiscussion/forgediscussion/utils.py
+++ ForgeDiscussion/forgediscussion/utils.py
@@ -21,40 +21,26 @@ def create_forum(app, new_forum):
     else:
         parent_id=None
         shortname = new_forum['shortname']
-    description = ''
-    if 'description' in new_forum:
-        description=new_forum['description']
-    if 'anon_posts' in new_forum:
-        anon_posts=new_forum['anon_posts']
-    else:
-        anon_posts = False
-    if 'members_only' in new_forum and new_forum['members_only']:
-        if anon_posts:
-            flash('You cannot have anonymous posts in a members only forum.', 'warning')
-            anon_posts = False
-        members_only=new_forum['members_only']
-    else:
-        members_only = False
+    description = new_forum.get('description','')
+
     f = DM.Forum(app_config_id=app.config._id,
                     parent_id=parent_id,
                     name=h.really_unicode(new_forum['name']),
                     shortname=h.really_unicode(shortname),
                     description=h.really_unicode(description),
-                    members_only=members_only)
-    if members_only:
+                    members_only=new_forum.get('members_only', False),
+                    anon_posts=new_forum.get('anon_posts', False),
+                    )
+    if f.members_only and f.anon_posts:
+        flash('You cannot have anonymous posts in a members only forum.', 'warning')
+        f.anon_posts = False
+    if f.members_only:
         role_developer = ProjectRole.by_name('Developer')._id
         f.acl = [
             ACE.allow(role_developer, ALL_PERMISSIONS),
             DENY_ALL]
-    else:
-        f.acl = []
-    role_anon = ProjectRole.anonymous()._id
-    if members_only:
-        role_developer = ProjectRole.by_name('Developer')._id
-        f.acl = [
-            ACE.allow(role_developer, ALL_PERMISSIONS),
-            DENY_ALL]
-    elif anon_posts:
+    elif f.anon_posts:
+        role_anon = ProjectRole.anonymous()._id
         f.acl = [ACE.allow(role_anon, 'post')]
     else:
         f.acl = []
