943e3a7c9509d7bd85763e50be819ca9e0fd59a1
Rick Copeland <rcopeland@geek.net>
2011-11-19 12:41:13 -0500

    Fix FUSE filesystem to handle path correctly
    Signed-off-by: Rick Copeland <rcopeland@geek.net>

--- fuse/accessfs.py
+++ fuse/accessfs.py
@@ -345,7 +345,7 @@ class PermissionCache(object):
         /SCM/project.neighborhood/a/b/c
         '''
         parts = [ p for p in path.split(os.path.sep) if p ]
-        nbhd, proj, rest = parts[0], parts[1], parts[2:]
+        scm, nbhd, proj, rest = parts[0], parts[1], parts[2], parts[3:]
         parts = ['/SCM/%s.%s' % (proj, nbhd) ] + rest
         return '/'.join(parts)
 
