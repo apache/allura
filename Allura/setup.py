8b9336f539f17c313191830f6c1f0ee3def9c3ba
Anton Kasyanov <mind_master@ukr.net>
2012-02-21 23:21:45 +0200

    ticket:3 added missed files

--- Allura/setup.py
+++ Allura/setup.py
@@ -119,6 +119,7 @@ setup(
     smtp_server=allura.command:SMTPServerCommand
     create-neighborhood = allura.command:CreateNeighborhoodCommand
     create-trove-categories = allura.command:CreateTroveCategoriesCommand
+    set-neighborhood-level = allura.command:SetNeighborhoodLevel
     
     [easy_widgets.resources]
     ew_resources=allura.config.resources:register_ew_resources
