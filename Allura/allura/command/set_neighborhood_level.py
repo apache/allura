45d29c56d1f62187e80e6c9906356864047f7665
Yaroslav Luzin <jardev@gmail.com>
2012-03-16 00:14:13 +0200

    ticket:8 Use neighrborhood's name as well as id in set-neighborhood-level command

--- Allura/allura/command/set_neighborhood_level.py
+++ Allura/allura/command/set_neighborhood_level.py
@@ -10,8 +10,9 @@ from ming.orm import session
 class SetNeighborhoodLevelCommand(base.Command):
     min_args=3
     max_args=3
-    usage = '<ini file> <neighborhood_id> <level>'  #not sure if we need ini file
-    summary = 'Change neighborhood level'
+    usage = '<ini file> <neighborhood> <level>'  #not sure if we need ini file
+    summary = 'Change neighborhood level. <neighgborhood> - '
+              'should neightborhood\'s name or id'
     parser = base.Command.standard_parser(verbose=True)
 
     def command(self):
@@ -21,8 +22,15 @@ class SetNeighborhoodLevelCommand(base.Command):
         if n_level not in ["silver", "gold", "platinum"]:
             base.log.error("You must select one of three level types (silver, gold, or platinum)")
             return
-        n = M.Neighborhood.query.get(_id = ObjectId(n_id))
-        n.level = n_level
-        if n_level == "gold":
-            n.migrate_css_for_gold_level()
-        session(M.Neighborhood).flush()
+
+        n = M.Neighborhood.query.get(name=n_id)
+        if not n:
+            n = M.Neighborhood.query.get(_id=ObjectId(n_id))
+
+        if not n:
+            base.log.error("The neighborhood % scould not be found" % n_id)
+        else:
+            n.level = n_level
+            if n_level == "gold":
+                n.migrate_css_for_gold_level()
+            session(M.Neighborhood).flush()
