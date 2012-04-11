cc30ec7711fdd9ee50286f1a1519047f0b398545
Rick Copeland <rick@arborian.com>
2012-04-11 16:45:24 -0400

    [#3059] Refactor git fork/merge request tests
    Signed-off-by: Rick Copeland <rick@arborian.com>

--- ForgeGit/forgegit/tests/functional/test_controllers.py
+++ ForgeGit/forgegit/tests/functional/test_controllers.py
@@ -15,10 +15,10 @@ from allura.lib import helpers as h
 from allura.tests import decorators as td
 from alluratest.controller import TestController
 
-class TestRootController(TestController):
+class _TestCase(TestController):
 
     def setUp(self):
-        TestController.setUp(self)
+        super(_TestCase, self).setUp()
         self.setup_with_tools()
 
     @td.with_git
@@ -36,62 +36,7 @@ class TestRootController(TestController):
         ThreadLocalORMSession.flush_all()
         ThreadLocalORMSession.close_all()
 
-    def test_fork(self):
-        r = self.app.get('%sfork/' % c.app.repo.url())
-        assert '<input type="text" name="mount_point" value="test"/>' in r
-        assert '<input type="text" name="mount_label" value="test - Git"/>' in r
-
-        to_project = M.Project.query.get(shortname='test2', neighborhood_id=c.project.neighborhood_id)
-        mount_point = 'reponame'
-        r = self.app.post('/src-git/fork', params=dict(
-            project_id=str(to_project._id),
-            mount_point=mount_point,
-            mount_label='Test forked repository'))
-        assert "{status: 'error'}" not in str(r.follow())
-        cloned_from = c.app.repo
-        with h.push_context('test2', mount_point, neighborhood='Projects'):
-            c.app.repo.init_as_clone(
-                    cloned_from.full_fs_path,
-                    cloned_from.app.config.script_name(),
-                    cloned_from.full_fs_path)
-        r = self.app.get('/p/test2/%s' % mount_point).follow().follow().follow()
-        assert 'Clone of' in r
-        assert 'Test forked repository' in r
-        r = self.app.get('/src-git/').follow().follow()
-        assert 'Forks' in r
-
-    def test_merge_request(self):
-        to_project = M.Project.query.get(shortname='test2', neighborhood_id=c.project.neighborhood_id)
-        r = self.app.post('/src-git/fork', params=dict(
-            project_id=str(to_project._id),
-            mount_point='code'))
-        cloned_from = c.app.repo
-        with h.push_context('test2', 'code', neighborhood='Projects'):
-            c.app.repo.init_as_clone(
-                    cloned_from.full_fs_path,
-                    cloned_from.app.config.script_name(),
-                    cloned_from.full_fs_path)
-        r = self.app.get('/p/test2/code/').follow().follow()
-        assert 'Request Merge' in r
-        # Request Merge button only visible to repo admins
-        kw = dict(extra_environ=dict(username='test-user'))
-        r = self.app.get('/p/test2/code/', **kw).follow(**kw).follow(**kw)
-        assert 'Request Merge' not in r, r
-        # Request merge controller action only permitted for repo admins
-        r = self.app.get('/p/test2/code/request_merge', status=403, **kw)
-        r = self.app.get('/p/test2/code/request_merge')
-        assert 'Request merge' in r
-        # Merge request detail view
-        r = r.forms[0].submit().follow()
-        assert 'would like you to merge' in r
-        mr_num = r.request.url.split('/')[-2]
-        # Merge request list view
-        r = self.app.get('/p/test/src-git/merge-requests/')
-        assert 'href="%s/"' % mr_num in r
-        # Merge request status update
-        r = self.app.post('/p/test/src-git/merge-requests/%s/save' % mr_num,
-                          params=dict(status='rejected')).follow()
-        assert 'Merge Request #%s:  (rejected)' % mr_num in r, r
+class TestRootController(_TestCase):
 
     def test_index(self):
         resp = self.app.get('/src-git/').follow().follow()
@@ -187,28 +132,98 @@ class TestRootController(TestController):
         assert '</pre>' in content, content
 
 
-class TestRestController(TestController):
-    def setUp(self):
-        super(TestRestController, self).setUp()
-        self.setup_with_tools()
-
-    @td.with_git
-    def setup_with_tools(self):
-        h.set_context('test', 'src-git', neighborhood='Projects')
-        repo_dir = pkg_resources.resource_filename(
-            'forgegit', 'tests/data')
-        c.app.repo.fs_path = repo_dir
-        c.app.repo.status = 'ready'
-        c.app.repo.name = 'testgit.git'
-        ThreadLocalORMSession.flush_all()
-        ThreadLocalORMSession.close_all()
-        h.set_context('test', 'src-git', neighborhood='Projects')
-        c.app.repo.refresh()
-        ThreadLocalORMSession.flush_all()
-        ThreadLocalORMSession.close_all()
+class TestRestController(_TestCase):
 
     def test_index(self):
         self.app.get('/rest/p/test/src-git/', status=200)
 
     def test_commits(self):
         self.app.get('/rest/p/test/src-git/commits', status=200)
+
+class TestFork(_TestCase):
+
+    def setUp(self):
+        super(TestFork, self).setUp()
+        to_project = M.Project.query.get(
+            shortname='test2', neighborhood_id=c.project.neighborhood_id)
+        r = self.app.post('/src-git/fork', params=dict(
+                project_id=str(to_project._id),
+                mount_point='code',
+                mount_label='Test forked repository'))
+        assert "{status: 'error'}" not in str(r.follow())
+        cloned_from = c.app.repo
+        with h.push_context('test2', 'code', neighborhood='Projects'):
+            c.app.repo.init_as_clone(
+                    cloned_from.full_fs_path,
+                    cloned_from.app.config.script_name(),
+                    cloned_from.full_fs_path)
+
+    def _follow(self, r, **kw):
+        if r.status_int == 302:
+            print r.request.url
+        while r.status_int == 302:
+            print ' ==> 302 ==> %s' % r.location
+            r = r.follow(**kw)
+        return r
+
+    def _upstream_page(self, **kw):
+        r = self.app.get('/src-git/', **kw)
+        r = self._follow(r, **kw)
+        return r
+
+    def _fork_page(self, **kw):
+        r = self.app.get('/p/test2/code/', **kw)
+        r = self._follow(r, **kw)
+        return r
+
+    def _request_merge(self, **kw):
+        r = self.app.get('/p/test2/code/request_merge', **kw)
+        r = self._follow(r, **kw)
+        r = r.forms[0].submit()
+        r = self._follow(r, **kw)
+        mr_num = r.request.url.split('/')[-2]
+        assert mr_num.isdigit(), mr_num
+        return r, mr_num
+
+    def test_fork_form(self):
+        r = self.app.get('%sfork/' % c.app.repo.url())
+        assert '<input type="text" name="mount_point" value="test"/>' in r
+        assert '<input type="text" name="mount_label" value="test - Git"/>' in r
+
+    def test_fork_listed_in_parent(self):
+        assert 'Forks' in self._upstream_page()
+
+    def test_fork_display(self):
+        r = self._fork_page()
+        assert 'Clone of' in r
+        assert 'Test forked repository' in r
+
+    def test_merge_request_visible_to_admin(self):
+        assert 'Request Merge' in self._fork_page()
+
+    def test_merge_request_invisible_to_non_admin(self):
+        assert 'Request Merge' not in self._fork_page(
+            extra_environ=dict(username='test-user'))
+
+    def test_merge_action_available_to_admin(self):
+        self.app.get('/p/test2/code/request_merge')
+
+    def test_merge_action_unavailable_to_non_admin(self):
+        self.app.get(
+            '/p/test2/code/request_merge',
+            status=403, extra_environ=dict(username='test-user'))
+
+    def test_merge_request_detail_view(self):
+        r, mr_num = self._request_merge()
+        assert 'would like you to merge' in r, r.showbrowser()
+
+    def test_merge_request_list_view(self):
+        r, mr_num = self._request_merge()
+        r = self.app.get('/p/test/src-git/merge-requests/')
+        assert 'href="%s/"' % mr_num in r, r
+
+    def test_merge_request_update_status(self):
+        r, mr_num = self._request_merge()
+        r = self.app.post('/p/test/src-git/merge-requests/%s/save' % mr_num,
+                          params=dict(status='rejected')).follow()
+        assert 'Merge Request #%s:  (rejected)' % mr_num in r, r
