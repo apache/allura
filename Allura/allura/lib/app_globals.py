17f1b24e725bd6be501e465837d931c1b0eff55c
bolkimen <bolkimen@yahoo.com>
2012-05-02 12:32:07 +0300

    ticket:21 add code line link and highlight

--- Allura/allura/lib/app_globals.py
+++ Allura/allura/lib/app_globals.py
@@ -74,7 +74,7 @@ class Globals(object):
         self.oid_store = M.OpenIdStore()
 
         # Setup pygments
-        self.pygments_formatter = pygments.formatters.HtmlFormatter(
+        self.pygments_formatter = utils.LineAnchorCodeHtmlFormatter(
             cssclass='codehilite',
             linenos='inline')
 
