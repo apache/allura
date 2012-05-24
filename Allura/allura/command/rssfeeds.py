e6f23190c69120d668c3a2464a4defc88c256d4e
bolkimen <bolkimen@yahoo.com>
2012-05-24 18:25:21 +0300

    [#4254] ticket:64 add plain tag for markdown and add some fixes for rss export script

--- Allura/allura/command/rssfeeds.py
+++ Allura/allura/command/rssfeeds.py
@@ -1,9 +1,14 @@
+from time import mktime
+from datetime import datetime
+from HTMLParser import HTMLParser
+
 import feedparser
 import html2text
 from bson import ObjectId
 
 import base
 
+from ming.orm import session
 from pylons import c
 
 from allura import model as M
@@ -11,6 +16,100 @@ from forgeblog import model as BM
 from forgeblog import version
 from forgeblog.main import ForgeBlogApp 
 from allura.lib import exceptions
+
+
+class MDHTMLParser(HTMLParser):
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.NO_END_TAGS = ["area", "base", "basefont", "br", "col", "frame",
+                            "hr", "img", "input", "link", "meta", "param"]
+        self.CUSTTAG_OPEN = u"[plain]"
+        self.CUSTTAG_CLOSE = u"[/plain]"
+        self.result_doc = u""
+        self.custom_tag_opened = False
+
+    def handle_starttag(self, tag, attrs):
+        if self.custom_tag_opened:
+            self.result_doc = u"%s%s" % (self.result_doc, self.CUSTTAG_CLOSE)
+            self.custom_tag_opened = False
+
+        tag_text = u"<%s" % tag
+        for attr in attrs:
+            if attr[1].find('"'):
+                tag_text = u"%s %s='%s'" % (tag_text, attr[0], attr[1])
+            else:
+                tag_text = u'%s %s="%s"' % (tag_text, attr[0], attr[1])
+        if tag not in self.NO_END_TAGS:
+            tag_text = tag_text + ">"
+        else:
+            tag_text = tag_text + "/>"
+        self.result_doc = u"%s%s" % (self.result_doc, tag_text)
+
+    def handle_endtag(self, tag):
+        if tag not in self.NO_END_TAGS:
+            if self.custom_tag_opened:
+                self.result_doc = u"%s%s" % (self.result_doc, self.CUSTTAG_CLOSE)
+                self.custom_tag_opened = False
+
+            self.result_doc = u"%s</%s>" % (self.result_doc, tag)
+
+    def handle_data(self, data):
+        if len(data.split()) == 0:
+            self.result_doc = u"%s%s" % (self.result_doc, data)
+            return
+
+        res_data = ""
+        for line in data.split('\n'):
+            if self.custom_tag_opened:
+                res_data = u"%s%s" % (res_data, self.CUSTTAG_CLOSE)
+                self.custom_tag_opened = False
+
+            if len(line.split()) > 0:
+                res_data = u"%s\n%s%s" % (res_data, self.CUSTTAG_OPEN, line)
+                self.custom_tag_opened = True
+            else:
+                res_data = u"%s\n" % res_data
+
+        if data[-1:] == "\n" and self.custom_tag_opened:
+            res_data = u"%s%s" % (res_data, self.CUSTTAG_CLOSE)
+            self.custom_tag_opened = False
+
+        self.result_doc = u"%s%s" % (self.result_doc, res_data)
+
+    def handle_comment(self, data):
+        if self.custom_tag_opened:
+            self.result_doc = u"%s%s" % (self.result_doc, self.CUSTTAG_CLOSE)
+            self.custom_tag_opened = False
+
+        self.result_doc = u"%s<!-- %s -->" % (self.result_doc, data)
+
+    def handle_entityref(self, name):
+        if not self.custom_tag_opened:
+            self.result_doc = u"%s%s" % (self.result_doc, self.CUSTTAG_OPEN)
+            self.custom_tag_opened = True
+
+        self.result_doc = u"%s&%s;" % (self.result_doc, name)
+
+    def handle_charref(self, name):
+        if not self.custom_tag_opened:
+            self.result_doc = u"%s%s" % (self.result_doc, self.CUSTTAG_OPEN)
+            self.custom_tag_opened = True
+
+        self.result_doc = u"%s&%s;" % (self.result_doc, name)
+
+    def handle_decl(self, data):
+        if self.custom_tag_opened:
+            self.result_doc = u"%s%s" % (self.result_doc, self.CUSTTAG_CLOSE)
+            self.custom_tag_opened = False
+
+        self.result_doc = u"%s<!%s>" % (self.result_doc, data)
+
+    def close(self):
+        HTMLParser.close(self)
+
+        if self.custom_tag_opened:
+            self.result_doc = u"%s%s" % (self.result_doc, self.CUSTTAG_CLOSE)
+            self.custom_tag_opened = False
 
 
 class RssFeedsCommand(base.Command):
@@ -30,7 +129,11 @@ class RssFeedsCommand(base.Command):
         self.prepare_feeds()
         for appid in self.feed_dict:
             for feed_url in self.feed_dict[appid]:
+                # TODO remove me
+                feed_url = 'https://twitter.com/statuses/user_timeline/openatadobe.atom'
                 self.process_feed(appid, feed_url)
+                # TODO remove me
+                break
 
     def prepare_feeds(self):
         feed_dict = {}
@@ -74,11 +177,27 @@ class RssFeedsCommand(base.Command):
                 content = e.summary
 
             content = u'%s <a href="%s">link</a>' % (content, e.link)
-            content = html2text.html2text(content, e.link)
+            parser = MDHTMLParser()
+            parser.feed(content)
+            parser.close()
+            content = html2text.html2text(parser.result_doc, e.link)
+            #print content
 
-            post = BM.BlogPost(title=title, text=content, app_config_id=appid,
+            updated = datetime.fromtimestamp(mktime(e.updated_parsed))
+
+            base_slug = BM.BlogPost.make_base_slug(title, updated, feed_url)
+            b_count = BM.BlogPost.query.find(dict(slug=base_slug)).count()
+            if b_count == 0:
+                post = BM.BlogPost(title=title, text=content, timestamp=updated,
+                               app_config_id=appid,
                                tool_version={'blog': version.__version__},
-                               state='draft')
-            post.neighborhood_id=c.project.neighborhood_id
-            post.make_slug()
-            post.commit()
+                               state='published')
+                post.neighborhood_id=c.project.neighborhood_id
+                post.make_slug(source=feed_url)
+                post.commit()
+
+            # TODO remove me
+            break
+        session(BM.BlogPost).flush()
+
+# paster pull-rss-feeds development.ini -a 4facfec6610b271748000005
