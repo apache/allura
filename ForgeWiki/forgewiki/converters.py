fa63e01a228f0598f20948fe1de97317cb7e3f9a
Yaroslav Luzin <jardev@gmail.com>
2012-05-25 13:22:48 +0300

    [#4186] ticket:57 Fixed pyflakes errors

--- ForgeWiki/forgewiki/converters.py
+++ ForgeWiki/forgewiki/converters.py
@@ -2,13 +2,14 @@
 import html2text
 import bbcode
 # https://github.com/zikzakmedia/python-mediawiki.git
-from mediawiki import *
+from mediawiki import wiki2html
 
 html2text.BODY_WIDTH = 0
 
 
 def mediawiki2markdown(source):
-    p = bbcode.Parser(newline='\n', escape_html=False, replace_links=False, replace_cosmetic=False)
+    p = bbcode.Parser(newline='\n', escape_html=False, replace_links=False,
+        replace_cosmetic=False)
     cleanbb_text = p.format(source)
     wiki_content = wiki2html(cleanbb_text, True)
     markdown_text = html2text.html2text(wiki_content)
