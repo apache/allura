1429e3a4fc93bf749bedb1153d923295b4f0489c
bolkimen <bolkimen@yahoo.com>
2012-05-30 12:15:13 +0300

    [#4186] ticket:58 add wiki2makdown test

--- ForgeWiki/forgewiki/tests/test_converters.py
+++ ForgeWiki/forgewiki/tests/test_converters.py
@@ -1,8 +1,9 @@
-from pylons import c, g
-
 from nose.tools import assert_equal
 
 from alluratest.controller import setup_basic_test, setup_global_objects
+
+from pylons import c, g
+
 from allura.tests import decorators as td
 from forgewiki import converters
 
@@ -24,10 +25,12 @@ def test_mediawiki2markdown():
     assert "**bolded text**_italicized text_" in bbcode_output
 
     mediawiki_text = """
+'''bold''' ''italics''
 == Getting started ==
 * [http://www.mediawiki.org/wiki/Manual:Configuration_settings Configuration settings list]
 * [http://www.mediawiki.org/wiki/Manual:FAQ MediaWiki FAQ]
     """
     mediawiki_output = converters.mediawiki2markdown(mediawiki_text)
+    assert "**bold** _italics_" in mediawiki_output
     assert "## Getting started" in mediawiki_output
     assert "* [MediaWiki FAQ](http://www.mediawiki.org/wiki/Manual:FAQ)" in mediawiki_output
