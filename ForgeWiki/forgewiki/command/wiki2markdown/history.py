b44dbf9bdaf8b16b2dc822808e79b721ffd74b3e
bolkimen <bolkimen@yahoo.com>
2012-06-04 15:42:50 +0300

    [#4186] ticket:60 change log message

--- ForgeWiki/forgewiki/command/wiki2markdown/history.py
+++ ForgeWiki/forgewiki/command/wiki2markdown/history.py
@@ -69,10 +69,10 @@ class HistoryImportUnit(BaseImportUnit):
         projects = M.Project.query.find().all()
         for p in projects:
             self._export_pages(p)
-        allura_base.log.info("Export pages complete")
+        allura_base.log.info("Export history complete")
 
     def load(self):
         projects = M.Project.query.find().all()
         for p in projects:
             self._load_pages(p)
-        allura_base.log.info("Load pages complete")
+        allura_base.log.info("Load history complete")
