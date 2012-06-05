c963eaf3f23781c1ed2cc705d58575d7fad01c59
bolkimen <bolkimen@yahoo.com>
2012-06-05 18:35:41 +0300

    [#4186] ticket:61 fix test case

--- ForgeWiki/forgewiki/tests/test_wiki2markdown_attachments.py
+++ ForgeWiki/forgewiki/tests/test_wiki2markdown_attachments.py
@@ -1,11 +1,13 @@
 import os
+from cStringIO import StringIO
 
 from ming.orm.ormsession import ThreadLocalORMSession
 
 from alluratest.controller import setup_basic_test, setup_global_objects
 from forgewiki.command import wiki2markdown
 from allura import model as M
-from forgediscussion import model as DM
+from forgewiki import model as WM
+from pylons import c
 
 test_config = 'test.ini#main'
 
@@ -16,11 +18,19 @@ def setUp(self):
 
 def test_wiki2markdown_attachments():
     output_dir = u'/tmp/wiki2markdown'
+    projects = M.Project.query.find().all()
 
-    for p in DM.ForumPost.query.find().all():
-        p.text = "'''bold''' ''italics''"
+    for p in projects:
+        wiki_app = p.app_instance('wiki')
+        if wiki_app is None:
+            continue
+        c.app = wiki_app
+        pages = WM.Page.query.find(dict(app_config_id=wiki_app.config._id)).all()
+        for p in pages:
+            p.attach('foo.text', StringIO('Hello, world!'))
     ThreadLocalORMSession.flush_all()
     ThreadLocalORMSession.close_all()
+
     cmd = wiki2markdown.Wiki2MarkDownCommand('wiki2markdown')
     cmd.run([test_config, '--extract-only', '--output-dir', output_dir, 'attachments'])
     cmd.command()
@@ -28,8 +38,7 @@ def test_wiki2markdown_attachments():
     projects = M.Project.query.find().all()
     for p in projects:
         wiki_app = p.app_instance('wiki')
-        discussion_app = p.app_instance('discussion')
-        if discussion_app is None and wiki_app is None:
+        if wiki_app is None:
             continue
 
         pid = "%s" % p._id
@@ -39,5 +48,13 @@ def test_wiki2markdown_attachments():
     cmd = wiki2markdown.Wiki2MarkDownCommand('wiki2markdown')
     cmd.run([test_config, '--load-only', '--output-dir', '/tmp/wiki2markdown', 'attachments'])
     cmd.command()
-    for p in DM.ForumPost.query.find().all():
-        assert "**bold** _italics_" in p.text
+
+    for p in projects:
+        wiki_app = p.app_instance('wiki')
+        if wiki_app is None:
+            continue
+
+        pages = WM.Page.query.find(dict(app_config_id=wiki_app.config._id)).all()
+        for p in pages:
+            assert '[foo.text](' in p.text
+            assert 'attachment/foo.text)' in p.text
