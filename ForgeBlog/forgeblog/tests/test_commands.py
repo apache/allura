b92144957d63bd1a3337ab29556741f991a525a7
Cory Johns <johnsca@geek.net>
2012-06-21 19:04:42 +0000

    [#4345] Added tests for plaintext tag and preprocessor
    Signed-off-by: Cory Johns <johnsca@geek.net>

--- ForgeBlog/forgeblog/tests/test_commands.py
+++ ForgeBlog/forgeblog/tests/test_commands.py
@@ -1,4 +1,8 @@
+import pylons
+pylons.c = pylons.tmpl_context
+pylons.g = pylons.app_globals
 from pylons import c, g
+from nose.tools import assert_equal
 
 from ming.orm.ormsession import ThreadLocalORMSession
 
@@ -31,3 +35,107 @@ def test_pull_rss_feeds():
     cmd.run([test_config, '-a', tmp_app._id])
     cmd.command()
     assert len(BM.BlogPost.query.find({'app_config_id': tmp_app._id}).all()) > 0
+
+def test_plaintext_parser():
+    parser = rssfeeds.MDHTMLParser()
+    parser.feed(
+        '1. foo\n'
+        '\n'
+        '#foo bar <a href="baz">baz</a>\n'
+        'foo bar\n'
+        '\n'
+        '#foo bar <a href="baz">\n'
+        'baz\n'
+        '</a>\n'
+    )
+    parser.close()
+    assert_equal(parser.result_doc,
+        "[plain]1. foo[/plain]\n"
+        "\n"
+        "[plain]#foo bar [/plain]<a href='baz'>[plain]baz[/plain]</a>\n"
+        "[plain]foo bar[/plain]\n"
+        "\n"
+        "[plain]#foo bar [/plain]<a href='baz'>\n"
+        "[plain]baz[/plain]\n"
+        "</a>\n"
+    )
+
+def test_plaintext_parser_wrapped():
+    parser = rssfeeds.MDHTMLParser()
+    parser.feed(
+        '<p>1. foo</p>\n'
+        '\n'
+        '<p>\n'
+        '#foo bar <a href="baz">baz</a>\n'
+        'foo bar\n'
+        '</p>\n'
+        '\n'
+        '<p>#foo bar <a href="baz">\n'
+        'baz\n'
+        '</a></p>\n'
+    )
+    parser.close()
+    assert_equal(parser.result_doc,
+        "<p>[plain]1. foo[/plain]</p>\n"
+        "\n"
+        "<p>\n"
+        "[plain]#foo bar [/plain]<a href='baz'>[plain]baz[/plain]</a>\n"
+        "[plain]foo bar[/plain]\n"
+        "</p>\n"
+        "\n"
+        "<p>[plain]#foo bar [/plain]<a href='baz'>\n"
+        "[plain]baz[/plain]\n"
+        "</a></p>\n"
+    )
+
+def test_plaintext_preprocessor():
+    html = g.markdown.convert(
+        "[plain]1. foo[/plain]\n"
+        "\n"
+        "[plain]#foo bar [/plain]<a href='baz'>[plain]baz[/plain]</a>\n"
+        "[plain]foo bar[/plain]\n"
+        "\n"
+        "[plain]#foo bar [/plain]<a href='baz'>\n"
+        "[plain]baz[/plain]\n"
+        "</a>\n"
+    )
+    assert_equal(html,
+        '<div class="markdown_content">'
+        '1. foo\n'
+        '\n'
+        '<p>'
+        '#foo bar <a href="../baz">baz</a><br />'
+        'foo bar'
+        '</p><p>'
+        '#foo bar <a href="../baz"><br />'
+        'baz<br />'
+        '</a>'
+        '</p></div>'
+    )
+
+def test_plaintext_preprocessor_wrapped():
+    html = g.markdown.convert(
+        "<p>[plain]1. foo[/plain]</p>\n"
+        "\n"
+        "<p>\n"
+        "[plain]#foo bar [/plain]<a href='baz'>[plain]baz[/plain]</a>\n"
+        "[plain]foo bar[/plain]\n"
+        "</p>\n"
+        "\n"
+        "<p>[plain]#foo bar [/plain]<a href='baz'>\n"
+        "[plain]baz[/plain]\n"
+        "</a></p>\n"
+    )
+    assert_equal(html,
+        '<div class="markdown_content">'
+        '<p>1. foo</p>\n'
+        '\n'
+        '<p>\n'
+        '#foo bar <a href="../baz">baz</a>\n'
+        'foo bar\n'
+        '</p>\n'
+        '<p>#foo bar <a href="../baz">\n'
+        'baz\n'
+        '</a></p>\n'
+        '</div>'
+    )
