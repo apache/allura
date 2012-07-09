1ac34a59289dd45c3fedf097464ad1d083a7297c
Tim Van Steenburgh <tvansteenburgh@gmail.com>
2012-07-09 15:22:09 +0000

    [#4382] Fix to work with new scm model.
    Signed-off-by: Tim Van Steenburgh <tvansteenburgh@gmail.com>

--- ForgeSVN/forgesvn/controllers.py
+++ ForgeSVN/forgesvn/controllers.py
@@ -14,7 +14,7 @@ class BranchBrowser(repository.BranchBrowser):
     @with_trailing_slash
     def index(self, limit=None, page=0, count=0, **kw):
         latest = c.app.repo.latest(branch=self._branch)
-        if not latest or (latest.object_id.split(':')[1] == '1' and h.has_access(c.app, 'write')()):
+        if not latest or (latest._id.split(':')[1] == '1' and h.has_access(c.app, 'write')()):
             return dict(allow_fork=False, log=[])
         redirect(latest.url() + 'tree/')
 
