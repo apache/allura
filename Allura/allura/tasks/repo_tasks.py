23996afcecc89727330be3d8f6aeeb1b1f6c99f9
Jenny Steele <jsteele@geek.net>
2012-07-13 16:56:30 -0400

    [#4513] Changed repo clone email to more obviously show old url vs. new checkout command.

--- Allura/allura/tasks/repo_tasks.py
+++ Allura/allura/tasks/repo_tasks.py
@@ -41,8 +41,12 @@ def clone(
                 subject=u'SourceForge Repo Clone Complete',
                 message_id=h.gen_message_id(),
                 text=u''.join([
-                    u'Clone of repo %s in project %s from %s is complete. Your repo is now ready to use.\n'
-                ]) % (c.app.config.options.mount_point, c.project.shortname, cloned_from_url))
+                    u'Your cloned repository %s in project %s is now ready for use.\n\n',
+                    u'Old repository url: %s \n\n',
+                    u'New repository checkout command: %s \n\n',
+                    u'You and any other developers should do a fresh checkout using the ',
+                    u'new repository location.\n'
+                ]) % (c.app.config.options.mount_point, c.project.shortname, cloned_from_url, c.app.repo.clone_command('rw')))
     except:
         sendmail(
             destinations=['sfengineers@geek.net'],
