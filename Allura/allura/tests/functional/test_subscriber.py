75dc4650cbabf3a44d3327eb6ac7a4e3a5f62865
Dave Brondsema <dbrondsema@geek.net>
2012-07-16 14:51:46 +0000

    [#4449] more explicit page labels

--- Allura/allura/tests/functional/test_subscriber.py
+++ Allura/allura/tests/functional/test_subscriber.py
@@ -11,7 +11,7 @@ class TestSubscriber(TestController):
     def test_add_subscriber(self):
 
         response = self.app.get("/nf/admin/add_subscribers")
-        assert "<h1>Add Subscribers</h1>" in response
+        assert "<h1>Add Subscribers to Artifact</h1>" in response
 
         response = self.app.get('/u/test-admin/profile/feed')
         assert 'Recent posts by Test Admin' in response
