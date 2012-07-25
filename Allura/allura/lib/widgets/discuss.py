0f2aab64d0cc2e238855dd54ba1926178fb99d63
Yuriy <yuriyarhipovua@yandex.ru>
2012-07-25 04:09:41 +0400

    [#4573] ticket:126 Autoexpanding textarea doesn't expand at the right rate in Firefox

--- Allura/allura/lib/widgets/discuss.py
+++ Allura/allura/lib/widgets/discuss.py
@@ -125,7 +125,7 @@ class EditPost(ff.ForgeForm):
         fields = ew_core.NameList()
         fields.append(ffw.MarkdownEdit(
             name='text',
-            attrs={'style':'height:7em; width:97%'}))
+            attrs={'style':'height:7em; width:100%'}))
         fields.append(ew.HiddenField(name='forum', if_missing=None))
         if ew_core.widget_context.widget:
             # we are being displayed
