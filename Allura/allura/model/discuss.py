b988d8940aa862568833b6255aa2e463e14b6331
Leo Trubach <leotrubach@gmail.com>
2012-08-02 04:52:38 +0600

    And one more fix

--- Allura/allura/model/discuss.py
+++ Allura/allura/model/discuss.py
@@ -503,7 +503,8 @@ class Post(Message, VersionedArtifact, ActivityObject):
             self.thread.last_post_date,
             self.mod_date)
         self.thread.update_stats()
-        artifact.update_stats()
+        if hasattr(artifact, 'update_stats'):
+            artifact.update_stats()
         g.director.create_activity(author, 'posted', self, target=artifact,
                 related_nodes=[self.app_config.project])
 
