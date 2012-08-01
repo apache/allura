83701a117099d151b9421aba029fbe0096155443
Leo Trubach <leotrubach@gmail.com>
2012-08-02 05:42:45 +0600

    [#4620] ticket:135 Remove previous stuff

--- ForgeTracker/forgetracker/model/ticket.py
+++ ForgeTracker/forgetracker/model/ticket.py
@@ -433,18 +433,6 @@ class Ticket(VersionedArtifact, ActivityObject, VotableArtifact):
                 n.send_simple(monitoring_email)
         Feed.post(self, description)
 
-    def post_sent(self, post):
-        monitoring_email = c.app.config.options.get('TicketMonitoringEmail')
-        monitoring_type = c.app.config.options.get('TicketMonitoringType')
-        if monitoring_email and monitoring_type == 'AllTicketChanges':
-            artifact = post.thread.artifact or post.thread
-            n = Notification.query.get(_id=artifact.url() + post._id)
-            if not n:
-                subject = 'Comment to ticket %s' % self.ticket_num
-                n = Notification._make_notification(artifact, 'message',
-                                                      subject=subject, post=post)
-            n.send_simple(monitoring_email)
-
     def url(self):
         return self.app_config.url() + str(self.ticket_num) + '/'
 
