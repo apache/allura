b6daea38823d052369a9e85134c404bcff696ed2
Hendrik Brummermann <nhnb@users.sourceforge.net>
2012-08-09 21:37:57 +0200

    [#4654] adds support for suffixes to the link tool.
    This is useful to keep URLs working when replacing one
    of the other tools with a self hosted version
    (e. g. because of retirement of that other tool).

--- ForgeLink/forgelink/tests/functional/test_root.py
+++ ForgeLink/forgelink/tests/functional/test_root.py
@@ -14,3 +14,30 @@ class TestRootController(TestController):
         response.form.submit()
         redirected = self.app.get('/link/index').follow()
         assert redirected.request.url == 'http://www.google.com/'
+
+    @td.with_link
+    def test_root_with_url(self):
+        response = self.app.get('/admin/link/options', validate_chunk=True)
+        response.form['url'] = 'http://www.google.com/'
+        response.form.submit()
+        redirected = self.app.get('/link').follow()
+        assert redirected.request.url == 'http://www.google.com/'
+
+    @td.with_link
+    def test_root_suffix_with_url_slash(self):
+        response = self.app.get('/admin/link/options', validate_chunk=True)
+        response.form['url'] = 'http://www.google.com/'
+        response.form.submit()
+        redirected = self.app.get('/link/service')
+        # HACK: support for remote redirects is limited in follow()
+        assert 'http://www.google.com/service' in redirected
+
+    @td.with_link
+    def test_root_suffix_with_url_value(self):
+        response = self.app.get('/admin/link/options', validate_chunk=True)
+        response.form['url'] = 'http://www.google.de/search?q='
+        response.form.submit()
+        response = self.app.get('/link/help')
+        # HACK: support for remote redirects is limited in follow()
+        assert 'http://www.google.de/search?q=help' in response
+
