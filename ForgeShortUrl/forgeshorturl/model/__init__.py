22c09c565edd37f08924959bc660ec80e5802b3d
Igor Bondarenko <jetmind2@gmail.com>
2012-09-07 15:04:09 +0300

    [#4785] ticket:163 improve ShortUrl model
    * add upsert method
    * change index to unique_index
    * fix typo in db collection name

---
+++ ForgeShortUrl/forgeshorturl/model/__init__.py
@@ -0,0 +1,1 @@
+from .shorturl import ShortUrl
--- ForgeShortUrl/forgeshorturl/model/shorturl.py
+++ ForgeShortUrl/forgeshorturl/model/shorturl.py
@@ -1,6 +1,7 @@
+import pymongo
 from ming.orm.declarative import MappedClass
 from allura.model import project_orm_session
-from ming.orm import FieldProperty, ForeignIdProperty
+from ming.orm import FieldProperty, ForeignIdProperty, session
 from datetime import datetime
 from ming import schema
 from allura.model.auth import User
@@ -11,9 +12,9 @@ from allura.model.timeline import ActivityNode, ActivityObject
 class ShortUrl(MappedClass, ActivityNode, ActivityObject):
 
     class __mongometa__:
-        name = 'short_urs'
+        name = 'short_urls'
         session = project_orm_session
-        indexes = ['short_name']
+        unique_indexes = ['short_name']
 
     _id = FieldProperty(schema.ObjectId)
     url = FieldProperty(str)
@@ -28,3 +29,16 @@ class ShortUrl(MappedClass, ActivityNode, ActivityObject):
     @property
     def user(self):
         return User.query.get(_id=self.create_user)
+
+    @classmethod
+    def upsert(cls, shortname):
+        u = cls.query.get(short_name=shortname)
+        if u is not None:
+            return u
+        try:
+            u = cls(short_name=shortname)
+            session(u).flush(u)
+        except pymongo.errors.DuplicateKeyError:
+            session(u).expunge(u)
+            u = cls.query.get(short_name=shortname)
+        return u
