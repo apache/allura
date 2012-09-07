473db38f4e28889e08dcc2450d8607de79158185
Igor Bondarenko <jetmind2@gmail.com>
2012-09-07 17:48:45 +0300

    [#4785] ticket:163 fix test

--- ForgeShortUrl/forgeshorturl/tests/test_commands.py
+++ ForgeShortUrl/forgeshorturl/tests/test_commands.py
@@ -3,11 +3,8 @@ from forgeshorturl.command import migrate_urls
 from forgeshorturl.model import ShortUrl
 from allura import model as M
 from mock import patch, MagicMock, Mock
-import datetime
-import time
 
 test_config = 'test.ini#main'
-fmt = '%Y-%m-%d %H:%M'
 
 
 def setUp(self):
@@ -17,10 +14,6 @@ def setUp(self):
 
 
 class TableMock(MagicMock):
-
-    def _make_timestamp(self, date):
-        d = datetime.datetime.strptime(date, fmt)
-        return time.mktime(d.timetuple())
 
     def select(self):
         m = Mock()
@@ -34,8 +27,8 @@ class TableMock(MagicMock):
                 'url': 'http://google.com',
                 'description': 'Two\nlines',
                 'private': 'N',
-                'create_time': self._make_timestamp('2012-09-01 10:00'),
-                'edit_time': self._make_timestamp('2012-09-01 12:30'),
+                'create_time': 1,
+                'edit_time': 2,
                 'create_user': 1
             },
             {
@@ -43,8 +36,8 @@ class TableMock(MagicMock):
                 'url': 'http://yahoo.com',
                 'description': 'One line',
                 'private': 'Y',
-                'create_time': self._make_timestamp('2012-09-02 10:00'),
-                'edit_time': self._make_timestamp('2012-09-02 12:30'),
+                'create_time': 3,
+                'edit_time': 4,
                 'create_user': 1
             }
         ]
@@ -65,16 +58,10 @@ def test_migrate_urls():
     assert u.url == 'http://google.com'
     assert u.description == 'Two\nlines'
     assert not u.private
-    assert u.created == datetime.datetime.strptime('2012-09-01 10:00', fmt)
-    assert u.last_updated == (datetime.datetime
-                                      .strptime('2012-09-01 12:30', fmt))
     assert u.create_user == M.User.anonymous()._id
 
     u = ShortUrl.query.get(short_name='y')
     assert u.url == 'http://yahoo.com'
     assert u.description == 'One line'
     assert u.private
-    assert u.created == datetime.datetime.strptime('2012-09-02 10:00', fmt)
-    assert u.last_updated == (datetime.datetime
-                                      .strptime('2012-09-02 12:30', fmt))
     assert u.create_user == M.User.anonymous()._id
