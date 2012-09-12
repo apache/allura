554f00f82a03f3fedb970c5d83911c4cb1927a34
Cory Johns <johnsca@geek.net>
2012-09-12 17:29:41 +0000

    [#4896] Make re_path_portion_fragment actually keep 1-character fragments
    Signed-off-by: Cory Johns <johnsca@geek.net>

--- Allura/allura/lib/helpers.py
+++ Allura/allura/lib/helpers.py
@@ -36,7 +36,7 @@ from allura.lib.decorators import exceptionless
 from allura.lib import AsciiDammit
 from .security import has_access
 
-re_path_portion_fragment = re.compile(r'[a-z][-a-z0-9]+')
+re_path_portion_fragment = re.compile(r'[a-z][-a-z0-9]*')
 re_path_portion = re.compile(r'^[a-z][-a-z0-9]{2,}$')
 re_clean_vardec_key = re.compile(r'''\A
 ( # first part
