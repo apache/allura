948d65b0d3c6757524c467ed1895d14c4372bbd5
Peter Hartmann <peterhartmann@apache.org>
2012-12-10 21:31:35 +0100

    [#4655] Remove ForgeHg from codebase.
    Signed-off-by: Peter Hartmann <peterhartmann@apache.org>

--- ForgeHg/forgehg/tests/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-## Make our own Mercurial tool test decorator
-from allura.tests.decorators import with_tool
-
-with_hg = with_tool('test', 'Hg', 'src-hg', 'Mercurial', type='hg')
