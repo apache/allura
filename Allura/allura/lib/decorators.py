0a0ba7a0ef81f1722af5d27e3cec89da66de5f4f
Cory Johns <johnsca@geek.net>
2013-02-14 19:03:45 +0000

    [#5703] Refactored task decorator's handling of class tasks
    Signed-off-by: Cory Johns <johnsca@geek.net>

--- Allura/allura/lib/decorators.py
+++ Allura/allura/lib/decorators.py
@@ -13,21 +13,14 @@ from webob import exc
 
 def task(func):
     '''Decorator to add some methods to task functions'''
-    if inspect.isclass(func):
-        return taskclass(func)
     def post(*args, **kwargs):
         from allura import model as M
         delay = kwargs.pop('delay', 0)
         return M.MonQTask.post(func, args, kwargs, delay=delay)
-    func.post = post
+    # if decorating a class, have to make it a staticmethod
+    # or it gets a spurious cls argument
+    func.post = staticmethod(post) if inspect.isclass(func) else post
     return func
-
-def taskclass(cls):
-    def post(*args, **kwargs):
-        from allura import model as M
-        return M.MonQTask.post(cls, args[1:], kwargs)
-    cls.post = classmethod(post)
-    return cls
 
 class event_handler(object):
     '''Decorator to register event handlers'''
