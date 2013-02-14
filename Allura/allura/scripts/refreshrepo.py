92cf227a5faece7987a1b38202022111fdae8339
Cory Johns <johnsca@geek.net>
2013-02-14 19:08:04 +0000

    [#5703] Removed clean_all option missed during merge conflict resolution
    Signed-off-by: Cory Johns <johnsca@geek.net>

--- Allura/allura/scripts/refreshrepo.py
+++ Allura/allura/scripts/refreshrepo.py
@@ -28,15 +28,6 @@ class RefreshRepo(ScriptTask):
             q_project['shortname'] = {'$regex': options.project_regex}
 
         log.info('Refreshing repositories')
-        if options.clean_all:
-            log.info('Removing all repository objects')
-            M.repo.CommitDoc.m.remove({})
-            M.repo.TreeDoc.m.remove({})
-            M.repo.TreesDoc.m.remove({})
-            M.repo.DiffInfoDoc.m.remove({})
-            M.repo.CommitRunDoc.m.remove({})
-            M.repo.LastCommitDoc.m.remove({})
-
         for chunk in chunked_find(M.Project, q_project):
             for p in chunk:
                 log.info("Refreshing repos for project '%s'." % p.shortname)
