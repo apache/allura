cb0892c162cf195986ecaab6a21840cf5fcf2ee4
Dave Brondsema <dbrondsema@geek.net>
2013-02-20 19:59:34 +0000

    Revert "[#5358] allow markdown escapes within autolinked URLs" since it breaks [a](http://a.com)
    This reverts commit bdd590a14134471c7e487f3cff1b9d1727d48148.

--- Allura/allura/tests/test_globals.py
+++ Allura/allura/tests/test_globals.py
@@ -260,18 +260,14 @@ def foo(): pass
 
 
 def test_markdown_autolink():
-    assert '<a href=' in g.markdown.convert('This is http://sf.net')
+    #with h.set_context('test', 'wiki', neighborhood='Projects')
+    text = g.markdown.convert('This is http://sf.net')
+    assert '<a href=' in text, text
     tgt = 'http://everything2.com/?node=nate+oostendorp'
     s = g.markdown.convert('This is %s' % tgt)
     assert_equal(
         s, '<div class="markdown_content"><p>This is <a href="%s" rel="nofollow">%s</a></p></div>' % (tgt, tgt))
-
-
-def test_markdown_autolink_with_escape():
-    # \_ is unnecessary but valid markdown escaping and should be considered as a regular underscore
-    # (it occurs during html2text conversion during project migrations)
-    r = g.markdown.convert('a http://www.phpmyadmin.net/home\_page/security/\#target b')
-    assert 'href="http://www.phpmyadmin.net/home_page/security/#target"' in r, r
+    assert '<a href=' in g.markdown.convert('This is http://sf.net')
 
 
 def test_macro_projects():
