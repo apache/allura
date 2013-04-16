fe2b21132e35bc72032269728b044de26d003a3c
Tim Van Steenburgh <tvansteenburgh@gmail.com>
2013-04-16 02:47:46 +0000

    Use custom Mock since mock lib not available
    Signed-off-by: Tim Van Steenburgh <tvansteenburgh@gmail.com>

--- Allura/docs/conf.py
+++ Allura/docs/conf.py
@@ -29,14 +29,31 @@
 # serve to show the default.
 
 import sys, os
-import mock
+
+class Mock(object):
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        return Mock()
+
+    @classmethod
+    def __getattr__(cls, name):
+        if name in ('__file__', '__path__'):
+            return '/dev/null'
+        elif name[0] == name[0].upper():
+            mockType = type(name, (), {})
+            mockType.__module__ = __name__
+            return mockType
+        else:
+            return Mock()
 
 MOCK_MODULES = ['matplotlib', 'matplotlib.axes',
         'matplotlib.backends', 'matplotlib.backends.backend_agg',
         'matplotlib.figure', 'matplotlib.patches']
 
 for mod_name in MOCK_MODULES:
-    sys.modules[mod_name] = mock.Mock()
+    sys.modules[mod_name] = Mock()
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
