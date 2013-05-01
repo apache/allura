75adfcc0785a592ce4ced6dba8c66c4421283e87
Igor Bondarenko <jetmind2@gmail.com>
2013-05-01 09:16:50 +0000

    [#5656] ticket:301 Change mass-edit.js to process move form also

--- ForgeTracker/forgetracker/widgets/resources/js/mass-edit.js
+++ ForgeTracker/forgetracker/widgets/resources/js/mass-edit.js
@@ -18,6 +18,13 @@
 */
 
 $(function(){
+    $form = $('#update-values')
+    if ($form.length == 0) {
+        $form = $('.editbox > form');
+    }
+    if ($('#id_search').length == 0) {
+        $form.append('<input type="hidden" name="__search" id="id_search">');
+    }
     $('#id_search').val(window.location.search);
     $('#assigned_to').val('');
     $('#select_all').click(function(){
@@ -28,16 +35,16 @@ $(function(){
             $('tbody.ticket-list input[type=checkbox]').removeAttr('checked');
         }
     });
-    $('#update-values').submit(function(){
+    $form.submit(function(){
         var $checked=$('tbody.ticket-list input:checked'), count=$checked.length;
 
         if ( !count ) {
-            $('#result').text('No tickets selected for update.');
+            $('#result').text('No tickets selected.');
             return false;
         }
 
         $checked.each(function() {
-            $('#update-values').append('<input type="hidden" name="__ticket_ids" value="'+$(this).val()+'"/>');
+            $form.append('<input type="hidden" name="__ticket_ids" value="'+$(this).val()+'"/>');
         });
     });
 });
