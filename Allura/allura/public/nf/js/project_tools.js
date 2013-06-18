eb7b35e346707f5585ec90ebb76342576724f64d
Dave Brondsema <dbrondsema@slashdotmedia.com>
2013-06-18 12:00:13 -0400

    s/non reversable/irreversable/g

--- Allura/allura/public/nf/js/project_tools.js
+++ Allura/allura/public/nf/js/project_tools.js
@@ -85,7 +85,7 @@
         if (mount_point) {
             tool_label = 'the "' + mount_point + '"';
         }
-        $('div.warning_msg').text('Warning: This will destroy all data in ' + tool_label + ' tool and is non-reversable!');
+        $('div.warning_msg').text('Warning: This will destroy all data in ' + tool_label + ' tool and is irreversable!');
 
         form_to_delete = this.parentNode;
         return false;
