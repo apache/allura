66f891aa8b27a3ce7c62b79b2d5faeb0d3ba554e
Dave Brondsema <dave@brondsema.net>
2014-04-12 13:51:31 -0400

    [#7305] add backwards compat repo.py back in

---
+++ Allura/allura/model/repo.py
@@ -0,0 +1,26 @@
+#       Licensed to the Apache Software Foundation (ASF) under one
+#       or more contributor license agreements.  See the NOTICE file
+#       distributed with this work for additional information
+#       regarding copyright ownership.  The ASF licenses this file
+#       to you under the Apache License, Version 2.0 (the
+#       "License"); you may not use this file except in compliance
+#       with the License.  You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#       Unless required by applicable law or agreed to in writing,
+#       software distributed under the License is distributed on an
+#       "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+#       KIND, either express or implied.  See the License for the
+#       specific language governing permissions and limitations
+#       under the License.
+
+
+# For backwards compatibility.
+# These used to be separate in this repo module, now all in repository module
+
+from .repository import SUser, SObjType
+from .repository import QSIZE, README_RE, VIEWABLE_EXTENSIONS, PYPELINE_EXTENSIONS, DIFF_SIMILARITY_THRESHOLD
+from .repository import CommitDoc, TreeDoc, LastCommitDoc, TreesDoc, DiffInfoDoc, CommitRunDoc
+from .repository import RepoObject, Commit, Tree, Blob, LastCommit
+from .repository import ModelCache
