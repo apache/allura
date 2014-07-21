d5d74a29e577064a58a0fcbaa3cf9b4a7da37fcd
Igor Bondarenko <jetmind2@gmail.com>
2014-07-21 15:07:06 +0300

    [#7524] ticket:613 Site admin page for users audit log

--- Allura/allura/model/auth.py
+++ Allura/allura/model/auth.py
@@ -903,6 +903,10 @@ class AuditLog(object):
             message = message % kwargs
         return cls(project_id=project._id, user_id=user._id, url=url, message=message)
 
+    @classmethod
+    def for_user(cls, user):
+        return cls.query.find(dict(project_id=None, user_id=user._id))
+
 main_orm_session.mapper(AuditLog, audit_log, properties=dict(
     project_id=ForeignIdProperty('Project'),
     project=RelationProperty('Project'),
