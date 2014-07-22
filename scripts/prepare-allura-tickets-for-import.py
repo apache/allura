9828de98139affa2ed0f9d5b684744566a7267e0
Dave Brondsema <dbrondsema@slashdotmedia.com>
2014-07-22 17:22:36 +0000

    [#7510] add license header and docstring to new script

--- scripts/prepare-allura-tickets-for-import.py
+++ scripts/prepare-allura-tickets-for-import.py
@@ -1,10 +1,32 @@
 #!/usr/bin/env python
+#       Licensed to the Apache Software Foundation (ASF) under one
+#       or more contributor license agreements.  See the NOTICE file
+#       distributed with this work for additional information
+#       regarding copyright ownership.  The ASF licenses this file
+#       to you under the Apache License, Version 2.0 (the
+#       "License"); you may not use this file except in compliance
+#       with the License.  You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#       Unless required by applicable law or agreed to in writing,
+#       software distributed under the License is distributed on an
+#       "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+#       KIND, either express or implied.  See the License for the
+#       specific language governing permissions and limitations
+#       under the License.
+
 from itertools import tee, izip, chain
 import json
 import git
 from collections import Counter
-from random import randint
 
+'''
+This script is for one-time conversion of Allura's own tickets from SourceForge
+host to forge-allura.apache.org hosting
+
+Change path variables here:
+'''
 
 filename = "/var/local/allura/tickets.json"  # Absolute path to exported tickets.json
 output = "/var/local/allura/updated_tickets.json"  # Absolute path to the output
