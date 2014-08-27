99de4f45c5cd348a7d422073adb2f7476d507801
Alexander Luberg <aluberg@slashdotmedia.com>
2014-08-27 05:28:00 +0000

    [#7527] Bugfix for migrations to prevent duplicates

--- scripts/migrations/030-email-address-_id-to-email--before-upgrade.js
+++ scripts/migrations/030-email-address-_id-to-email--before-upgrade.js
@@ -2,5 +2,5 @@ db.email_address.find().snapshot().forEach(function (e) {
     e.email = e._id;
     e._id = new ObjectId();
     db.email_address_new.insert(e);
-    db.email_address.update({'_id': e._id}, {'migrated': true});
+    db.email_address.update({'_id': e.email}, {$set: {migrated: true}});
 });
--- scripts/migrations/030-email-address-_id-to-email--cleanup.js
+++ scripts/migrations/030-email-address-_id-to-email--cleanup.js
@@ -1,8 +1,8 @@
-db.email_address_old.find({'migrated': {'$ne': false}}).snapshot().forEach(function (e) {
+db.email_address_old.find({'migrated': {'$ne': true}}).snapshot().forEach(function (e) {
     e.email = e._id;
     e._id = new ObjectId();
     db.email_address.insert(e);
-    db.email_address_old.update({'_id': e._id}, {'migrated': true});
+    db.email_address_old.update({'_id': e.email}, {$set: {migrated: true}});
 });
 // Drop the collection manually if everything is okay
 // db.email_address_old.drop();
