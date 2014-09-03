da026b8388e023aeb283ec99aff03d198db0a99f
Igor Bondarenko <jetmind2@gmail.com>
2014-09-03 13:20:16 +0300

    [#7628] ticket:646 Remove duplicates from command

--- Allura/allura/command/create_trove_categories.py
+++ Allura/allura/command/create_trove_categories.py
@@ -40,7 +40,6 @@ class CreateTroveCategoriesCommand(base.Command):
     # To add new migration append it's name to following list,
     # and create method m__<migration_name>
     migrations = [
-        'add_agpl_and_lppl',
         'sync',
         'set_parent_only',
         'add_license',
@@ -1458,68 +1457,6 @@ class CreateTroveCategoriesCommand(base.Command):
             getattr(self, 'm__' + name)()
             session(M.TroveCategory).flush()
 
-    def m__add_agpl_and_lppl(self):
-        M.TroveCategory(trove_cat_id=670,
-                        trove_parent_id=14,
-                        shortname="agpl",
-                        fullname="Affero GNU Public License",
-                        fullpath="License :: OSI-Approved Open Source :: Affero GNU Public License")
-        M.TroveCategory(trove_cat_id=862,
-                        trove_parent_id=14,
-                        shortname="lppl",
-                        fullname="LaTeX Project Public License",
-                        fullpath="License :: OSI-Approved Open Source :: LaTeX Project Public License")
-        M.TroveCategory(trove_cat_id=655,
-                        trove_parent_id=432,
-                        shortname="win64",
-                        fullname="64-bit MS Windows",
-                        fullpath="Operating System :: Grouping and Descriptive Categories :: 64-bit MS Windows")
-        M.TroveCategory(trove_cat_id=657,
-                        trove_parent_id=418,
-                        shortname="vista",
-                        fullname="Vista",
-                        fullpath="Operating System :: Modern (Vendor-Supported) Desktop Operating Systems :: Vista")
-        M.TroveCategory(trove_cat_id=851,
-                        trove_parent_id=418,
-                        shortname="win7",
-                        fullname="Windows 7",
-                        fullpath="Operating System :: Modern (Vendor-Supported) Desktop Operating Systems :: Windows 7")
-        M.TroveCategory(trove_cat_id=728,
-                        trove_parent_id=315,
-                        shortname="android",
-                        fullname="Android",
-                        fullpath="Operating System :: Handheld/Embedded Operating Systems :: Android")
-        M.TroveCategory(trove_cat_id=780,
-                        trove_parent_id=315,
-                        shortname="ios",
-                        fullname="Apple iPhone",
-                        fullpath="Operating System :: Handheld/Embedded Operating Systems :: Apple iPhone")
-        M.TroveCategory(trove_cat_id=863,
-                        trove_parent_id=534,
-                        shortname="architects",
-                        fullname="Architects",
-                        fullpath="Intended Audience :: by End-User Class :: Architects")
-        M.TroveCategory(trove_cat_id=864,
-                        trove_parent_id=534,
-                        shortname="auditors",
-                        fullname="Auditors",
-                        fullpath="Intended Audience :: by End-User Class :: Auditors")
-        M.TroveCategory(trove_cat_id=865,
-                        trove_parent_id=534,
-                        shortname="testers",
-                        fullname="Testers",
-                        fullpath="Intended Audience :: by End-User Class :: Testers")
-        M.TroveCategory(trove_cat_id=866,
-                        trove_parent_id=534,
-                        shortname="secpros",
-                        fullname="Security Professionals",
-                        fullpath="Intended Audience :: by End-User Class :: Security Professionals")
-        M.TroveCategory(trove_cat_id=867,
-                        trove_parent_id=535,
-                        shortname="secindustry",
-                        fullname="Security",
-                        fullpath="Intended Audience :: by Industry or Sector :: Security")
-
     def m__sync(self):
         self.create_trove_cat(
             (639, 14, "cpal", "Common Public Attribution License 1.0 (CPAL)",
@@ -1604,12 +1541,6 @@ class CreateTroveCategoriesCommand(base.Command):
         self.create_trove_cat(
             (678, 14, "boostlicense", "Boost Software License (BSL1.0)",
              "License :: OSI-Approved Open Source :: Boost Software License (BSL1.0)"))
-        self.create_trove_cat(
-            (679, 14, "gplv3", "GNU General Public License version 3.0 (GPLv3)",
-             "License :: OSI-Approved Open Source :: GNU General Public License version 3.0 (GPLv3)"))
-        self.create_trove_cat(
-            (680, 14, "lgplv3", "GNU Library or ""Lesser"" General Public License version 3.0 (LGPLv3)",
-             "License :: OSI-Approved Open Source :: GNU Library or ""Lesser"" General Public License version 3.0 (LGPLv3)"))
         self.create_trove_cat(
             (681, 14, "isclicense", "ISC License", "License :: OSI-Approved Open Source :: ISC License"))
         self.create_trove_cat((682, 14, "multicslicense", "Multics License",
