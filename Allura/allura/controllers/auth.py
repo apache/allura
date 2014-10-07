bae9adb2f3a682b8f40a38f4b8b2f8b1cdd734b9
Aleksey 'LXj' Alekseyev <gotletter@gmail.com>
2014-10-07 20:45:43 +0300

    [#7704] ticket:662 Clear pending status on confirming an email address

--- Allura/allura/controllers/auth.py
+++ Allura/allura/controllers/auth.py
@@ -266,7 +266,11 @@ class AuthController(BaseController):
             })
 
             flash('Email address confirmed')
-            h.auditlog_user('Email address verified: %s', addr.email, user=addr.claimed_by_user())
+            h.auditlog_user('Email address verified: %s',  addr.email, user=addr.claimed_by_user())
+
+            if addr.claimed_by_user.pending:
+                addr.claimed_by_user.pending = False
+                addr.claimed_by_user.m.save()
         else:
             flash('Unknown verification link', 'error')
 
