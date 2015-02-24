0180e68a530da411c32aac7fbbd573b7805884e0
Igor Bondarenko <jetmind2@gmail.com>
2015-02-24 11:15:43 +0000

    [#7827] ticket:734 Get rid of (not used) tooltip

--- Allura/allura/lib/widgets/project_list.py
+++ Allura/allura/lib/widgets/project_list.py
@@ -83,27 +83,6 @@ class ProjectSummary(ew_core.Widget):
 
         return response
 
-    def resources(self):
-        yield ew.JSLink('js/jquery.tools.min.js')
-        yield ew.JSScript('''
-        $(document).ready(function () {
-            var badges = $('small.badge');
-            var i = badges.length;
-            while (i) {
-                i--;
-                var tipHolder = document.createElement('div');
-                tipHolder.id = "tip" + i;
-                tipHolder.className = "tip";
-                document.body.appendChild(tipHolder);
-                $(badges[i]).parent('a[title]').tooltip({
-                    tip: '#tip' + i,
-                    opacity: '.9',
-                    offset: [-10, 0]
-                });
-            }
-        });
-        ''')
-
 
 class ProjectList(ew_core.Widget):
     template = 'jinja:allura:templates/widgets/project_list_widget.html'
