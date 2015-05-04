6f6139d1e1c894a7a3115cdd5acba6ce9a6e1dfc
Igor Bondarenko <jetmind2@gmail.com>
2015-05-04 17:30:13 +0000

    [#7868] ticket:759 Add test for phone service

---
+++ Allura/allura/tests/unit/phone/test_nexmo.py
@@ -0,0 +1,136 @@
+#       Licensed to the Apache Software Foundation (ASF) under one
+#       or more contributor license agreements.  See the NOTICE file
+#       distributed with this work for additional information
+#       regarding copyright ownership.  The ASF licenses this file
+#       to you under the Apache License, Version 2.0 (the
+#       "License"); you may not use this file except in compliance
+#       with the License.  You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#       Unless required by applicable law or agreed to in writing,
+#       software distributed under the License is distributed on an
+#       "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+#       KIND, either express or implied.  See the License for the
+#       specific language governing permissions and limitations
+#       under the License.
+
+import json
+from mock import patch
+from datadiff.tools import assert_equal
+
+from allura.lib.phone.nexmo import NexmoPhoneService
+
+
+class TestPhoneService(object):
+
+    def setUp(self):
+        config = {'phone.api_key': 'test-api-key',
+                  'phone.api_secret': 'test-api-secret',
+                  'site_name': 'Very loooooooooong site name'}
+        self.phone = NexmoPhoneService(config)
+
+    def test_add_common_params(self):
+        params = {'number': '1234567890', 'brand': 'Allura'}
+        res = self.phone.add_common_params(params)
+        expected = {'number': '1234567890',
+                    'brand': 'Allura',
+                    'api_key': 'test-api-key',
+                    'api_secret': 'test-api-secret'}
+        assert_equal(expected, res)
+
+    def test_error(self):
+        res = self.phone.error('text')
+        expected = {'status': 'error', 'error': 'text'}
+        assert_equal(expected, res)
+
+    def test_ok(self):
+        res = self.phone.ok(request_id='123', other='smth')
+        expected = {'status': 'ok', 'request_id': '123', 'other': 'smth'}
+        assert_equal(expected, res)
+
+    @patch('allura.lib.phone.nexmo.requests', autospec=True)
+    def test_verify(self, req):
+        req.post.return_value.json.return_value = {
+            'request_id': 'test-req-id',
+            'status': '0',
+        }
+        data = json.dumps({
+            'number': '1234567890',
+            'api_key': 'test-api-key',
+            'api_secret': 'test-api-secret',
+            'brand': 'Very loooooooooong',
+        }, sort_keys=True)
+        headers = {'Content-Type': 'application/json'}
+
+        resp = self.phone.verify('1234567890')
+        expected = {'status': 'ok', 'request_id': 'test-req-id'}
+        assert_equal(expected, resp)
+        req.post.assert_called_once_with(
+            'https://api.nexmo.com/verify/json',
+            data=data,
+            headers=headers)
+
+        req.post.reset_mock()
+        req.post.return_value.json.return_value = {
+            'status': '1',
+            'error_text': 'Something went wrong',
+        }
+        resp = self.phone.verify('1234567890')
+        expected = {'status': 'error', 'error': 'Something went wrong'}
+        assert_equal(expected, resp)
+        req.post.assert_called_once_with(
+            'https://api.nexmo.com/verify/json',
+            data=data,
+            headers=headers)
+
+    @patch('allura.lib.phone.nexmo.requests', autospec=True)
+    def test_verify_exception(self, req):
+        req.post.side_effect = Exception('Boom!')
+        resp = self.phone.verify('1234567890')
+        expected = {'status': 'error',
+                    'error': 'Failed sending request to Nexmo'}
+        assert_equal(expected, resp)
+
+    @patch('allura.lib.phone.nexmo.requests', autospec=True)
+    def test_check(self, req):
+        req.post.return_value.json.return_value = {
+            'request_id': 'test-req-id',
+            'status': '0',
+        }
+        data = json.dumps({
+            'request_id': 'test-req-id',
+            'code': '1234',
+            'api_key': 'test-api-key',
+            'api_secret': 'test-api-secret',
+        }, sort_keys=True)
+        headers = {'Content-Type': 'application/json'}
+
+        resp = self.phone.check('test-req-id', '1234')
+        expected = {'status': 'ok', 'request_id': 'test-req-id'}
+        assert_equal(expected, resp)
+        req.post.assert_called_once_with(
+            'https://api.nexmo.com/verify/check/json',
+            data=data,
+            headers=headers)
+
+        req.post.reset_mock()
+        req.post.return_value.json.return_value = {
+            'status': '1',
+            'error_text': 'Something went wrong',
+        }
+        resp = self.phone.check('test-req-id', '1234')
+        expected = {'status': 'error', 'error': 'Something went wrong'}
+        assert_equal(expected, resp)
+        req.post.assert_called_once_with(
+            'https://api.nexmo.com/verify/check/json',
+            data=data,
+            headers=headers)
+
+    @patch('allura.lib.phone.nexmo.requests', autospec=True)
+    def test_check_exception(self, req):
+        req.post.side_effect = Exception('Boom!')
+        resp = self.phone.check('req-id', '1234')
+        expected = {'status': 'error',
+                    'error': 'Failed sending request to Nexmo'}
+        assert_equal(expected, resp)
