a0f20c230f29ce966b340c2e19740c16f825c73d
Igor Bondarenko <jetmind2@gmail.com>
2015-05-14 15:33:34 +0000

    [#7633] ticket:768 Add has_access API for ForgeDiscussion

--- ForgeDiscussion/forgediscussion/tests/functional/test_rest.py
+++ ForgeDiscussion/forgediscussion/tests/functional/test_rest.py
@@ -247,3 +247,43 @@ class TestRootRestController(TestDiscussionApiBase):
                          extra_environ={'username': '*anonymous'})
         assert_equal(len(r.json['forums']), 1)
         assert_equal(r.json['forums'][0]['shortname'], 'general')
+
+    def test_has_access_no_params(self):
+        r = self.api_get('/rest/p/test/discussion/has_access', status=404)
+        r = self.api_get('/rest/p/test/discussion/has_access?user=root', status=404)
+        r = self.api_get('/rest/p/test/discussion/has_access?perm=read', status=404)
+
+    def test_has_access_unknown_params(self):
+        """Unknown user and/or permission always False for has_access API"""
+        r = self.api_get(
+            '/rest/p/test/discussion/has_access?user=babadook&perm=read',
+            user='root')
+        assert_equal(r.status_int, 200)
+        assert_equal(r.json['result'], False)
+        r = self.api_get(
+            '/rest/p/test/discussion/has_access?user=test-user&perm=jump',
+            user='root')
+        assert_equal(r.status_int, 200)
+        assert_equal(r.json['result'], False)
+
+    def test_has_access_not_admin(self):
+        """
+        User which has no 'admin' permission on neighborhood can't use
+        has_access API
+        """
+        self.api_get(
+            '/rest/p/test/discussion/has_access?user=test-admin&perm=admin',
+            user='test-user',
+            status=403)
+
+    def test_has_access(self):
+        r = self.api_get(
+            '/rest/p/test/discussion/has_access?user=test-admin&perm=post',
+            user='root')
+        assert_equal(r.status_int, 200)
+        assert_equal(r.json['result'], True)
+        r = self.api_get(
+            '/rest/p/test/discussion/has_access?user=*anonymous&perm=admin',
+            user='root')
+        assert_equal(r.status_int, 200)
+        assert_equal(r.json['result'], False)
