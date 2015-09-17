656dfa307cc78bceb06f0b8fc63db6cd020f8115
Dave Brondsema <dbrondsema@slashdotmedia.com>
2015-09-17 19:57:05 +0000

    [#7955] fix test

--- ForgeDiscussion/forgediscussion/tests/functional/test_forum.py
+++ ForgeDiscussion/forgediscussion/tests/functional/test_forum.py
@@ -494,7 +494,7 @@ class TestForum(TestController):
 
         r = self.app.get(thread.request.url)
         assert '<div class="display_post moderate">' in r
-        assert '<a href="" class="reply_post btn" style="display:none">' in r
+        assert '<a href="#" class="reply_post btn" style="display:none">' in r
         assert r.html.find(
             'a', {'class': 'little_link shortlink', 'style': 'display:none'}) is not None
         assert 'name="delete"' in r
