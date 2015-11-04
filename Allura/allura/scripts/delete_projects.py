350d8f4dc4d3194a01c1051cba334ab6a566f14d
Igor Bondarenko <jetmind2@gmail.com>
2015-11-04 15:26:02 +0200

    [#7999] ticket:856 Disable users

--- Allura/allura/scripts/delete_projects.py
+++ Allura/allura/scripts/delete_projects.py
@@ -20,9 +20,11 @@ import logging
 
 from ming.odm import Mapper, session
 from pylons import app_globals as g
+from webob import Request
 
 from allura.scripts import ScriptTask
 from allura import model as M
+from allura.lib.plugin import AuthenticationProvider
 from allura.tasks.index_tasks import solr_del_project_artifacts
 
 
@@ -37,9 +39,7 @@ class DeleteProjects(ScriptTask):
             proj = cls.get_project(proj)
             if proj:
                 log.info('Purging %s%s. Reason: %s', proj.neighborhood.url_prefix, proj.shortname, options.reason)
-                pid = proj._id
-                cls.purge_project(proj)
-                g.post_event('project_deleted', project_id=pid, reason=options.reason)
+                cls.purge_project(proj, options)
 
     @classmethod
     def get_project(cls, proj):
@@ -55,20 +55,42 @@ class DeleteProjects(ScriptTask):
         return p
 
     @classmethod
-    def purge_project(cls, project):
+    def purge_project(cls, project, options):
         pid = project._id
         solr_del_project_artifacts.post(pid)
+        if options.disable_users:
+            # Disable users if necessary BEFORE removing all project-related documents
+            cls.disable_users(project, options)
         app_config_ids = [ac._id for ac in M.AppConfig.query.find(dict(project_id=pid))]
         for m in Mapper.all_mappers():
-            cls = m.mapped_class
+            mcls = m.mapped_class
             if 'project_id' in m.property_index:
                 # Purge the things directly related to the project
-                cls.query.remove(dict(project_id=pid))
+                mcls.query.remove(dict(project_id=pid))
             elif 'app_config_id' in m.property_index:
                 # ... and the things related to its apps
-                cls.query.remove(dict(app_config_id={'$in': app_config_ids}))
+                mcls.query.remove(dict(app_config_id={'$in': app_config_ids}))
         project.delete()
         session(project).flush()
+        g.post_event('project_deleted', project_id=pid, reason=options.reason)
+
+    @classmethod
+    def disable_users(cls, project, options):
+        provider = AuthenticationProvider.get(Request.blank('/'))
+        users = project.admins() + project.users_with_role('Developer')
+        for user in users:
+            if user.disabled:
+                continue
+            log.info(u'Disabling user %s', user.username)
+            provider.disable_user(user, audit=False)
+            msg = u'Account disabled because project {}{} is deleted. Reason: {}'.format(
+                project.neighborhood.url_prefix,
+                project.shortname,
+                options.reason)
+            M.AuditLog.log_user(msg, user=user)
+            # `users` can contain duplicates. Make sure changes are visible
+            # to next iterations, so that `user.disabled` check works.
+            session(user).expunge(user)
 
     @classmethod
     def parser(cls):
@@ -77,6 +99,8 @@ class DeleteProjects(ScriptTask):
                             help='Project to delete in a form nbhd_prefix/shortname')
         parser.add_argument('-r', '--reason', type=str,
                             help='Reason why these projects being deleted')
+        parser.add_argument('--disable-users', action='store_true', default=False,
+                            help='Disable all project members')
         return parser
 
 
