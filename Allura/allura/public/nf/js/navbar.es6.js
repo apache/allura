7cddfa3f8bcd188f100e46dbb0d3c1105b3ca11e
Dave Brondsema <dave@brondsema.net>
2015-11-23 15:36:51 -0500

    [#7919] include admin options when getting nav data; restore getNavJson call after reorder

--- Allura/allura/public/nf/js/navbar.es6.js
+++ Allura/allura/public/nf/js/navbar.es6.js
@@ -494,7 +494,7 @@ var Main = React.createClass({
      * When invoked, this updates the state with the latest data from the server.
      */
     getNavJson: function() {
-        $.post(`${_getProjectUrl(false)}/_nav.json`, function(result) {
+        $.post(`${_getProjectUrl(false)}/_nav.json?admin_options=1`, function(result) {
             if (this.isMounted()) {
                 this.setState({
                     data: result
@@ -577,6 +577,7 @@ var Main = React.createClass({
                         interval: 500,
                         timer: 2000
                     });
+                _this.getNavJson();
             },
 
             error: function() {
