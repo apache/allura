179bf1e38d0c8c6e94f521ce9d6f4f69a4e4954b
Dave Brondsema <dave@brondsema.net>
2015-11-23 14:44:29 -0500

    [#8027] update copyright year

--- ForgeImporters/docs/conf.py
+++ ForgeImporters/docs/conf.py
@@ -54,7 +54,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Apache Allura'
-copyright = '2012-2014 The Apache Software Foundation'
+copyright = '2012-2015 The Apache Software Foundation'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
