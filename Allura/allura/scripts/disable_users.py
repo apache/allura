672ac23b0cff2b3f59e15acc7e807f1ba8a73cf0
Dave Brondsema <dave@brondsema.net>
2016-03-04 13:20:42 -0500

    Add audit log messages to disable_users.py script

--- Allura/allura/scripts/disable_users.py
+++ Allura/allura/scripts/disable_users.py
@@ -22,6 +22,7 @@ import sys
 from ming.odm import session
 
 from allura.lib.plugin import AuthenticationProvider
+from allura.lib import helpers as h
 from allura.scripts import ScriptTask
 from allura import model as M
 
@@ -37,10 +38,10 @@ class DisableUsers(ScriptTask):
             usernames = [line.strip() for line in sys.stdin]
         else:
             usernames = options.usernames
-        cls.disable_users(usernames)
+        cls.disable_users(usernames, options.message)
 
     @classmethod
-    def disable_users(cls, usernames):
+    def disable_users(cls, usernames, message):
         auth_provider = AuthenticationProvider.get(request=None)
 
         # would be nice to use the BatchIndexer extension around this but that only works for artifacts not users
@@ -56,10 +57,15 @@ class DisableUsers(ScriptTask):
                 log.info('Disabling user: %s', username)
                 auth_provider.disable_user(user)
                 session(user).flush(user)
+                if message:
+                    log_entry = h.auditlog_user(message, user=user)
+                    session(log_entry).flush(log_entry)
 
     @classmethod
     def parser(cls):
         parser = argparse.ArgumentParser(description='Disable listed users')
+        parser.add_argument('--message', '-m', dest='message', type=str, default='Account disabled',
+                            help='Message to add to the audit log for each user (set to empty string to skip message altogether)')
         parser.add_argument(
             '--usernames', dest='usernames', type=str, nargs='+', metavar='<username>', required=True,
             help='List of usernames, or "-" to read from stdin')
