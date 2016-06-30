2dac879ffbc50e8614143d32adb096f1ad19550b
Rohan Verma <rohanverma2004@gmail.com>
2016-06-30 14:32:21 +0530

    Changed usage permission to read instead of write for refresh button

--- Allura/allura/controllers/repository.py
+++ Allura/allura/controllers/repository.py
@@ -468,9 +468,8 @@ class MergeRequestController(object):
 
     @expose()
     @require_post()
-    @validate(mr_dispose_form)
     def refresh(self, **kw):
-        require_access(self.req, 'write')
+        require_access(self.req, 'read')
         with self.req.push_downstream_context():
             self.req.downstream['commit_id'] = c.app.repo.commit(self.req.source_branch)._id
         redirect(self.req.url())
