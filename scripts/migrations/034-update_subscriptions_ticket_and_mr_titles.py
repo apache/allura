7e127c75914944db888d79cb5ea7cc269569579d
Dave Brondsema <dave@brondsema.net>
2018-10-26 14:47:03 -0400

    [#8081] safeguards/performance tweaks for running on a large database

--- scripts/migrations/034-update_subscriptions_ticket_and_mr_titles.py
+++ scripts/migrations/034-update_subscriptions_ticket_and_mr_titles.py
@@ -20,6 +20,8 @@ import re
 import sys
 from pylons import tmpl_context as c
 from bson import ObjectId
+
+from ming.odm import session
 from ming.orm import ThreadLocalORMSession
 
 from allura import model as M
@@ -33,28 +35,33 @@ def main():
     c.project = None
 
     # Fix ticket artifcat titles
-    title = re.compile('(Ticket [0-9]+.*)')
+    title = re.compile('^Ticket [0-9]')
     subs_tickets = M.Mailbox.query.find(dict(artifact_title=title)).all()
     print 'Found total %d old artifact titles (tickets).' % len(subs_tickets)
     for sub in subs_tickets:
         ticket = TM.Ticket.query.get(_id = ObjectId(sub.artifact_index_id.split('#')[1]))
+        if not ticket:
+            print 'Could not find ticket for %s' % sub
         new_title = 'Ticket #%d: %s' % (ticket.ticket_num, ticket.summary)
         print '"%s" --> "%s"' % (sub.artifact_title , new_title)
         if(task != 'diff'):
             sub.artifact_title = new_title
+        session(sub).flush(sub)
 
     # Fix merge request artifact titles
-    title = re.compile('(Merge request: .*)')
+    title = re.compile('^Merge request: ')
     subs_mrs = M.Mailbox.query.find(dict(artifact_title=title)).all()
     print 'Found total %d old artifact titles (merge_requests).' % len(subs_tickets)
     for sub in subs_mrs:
         merge_request = M.MergeRequest.query.get(_id = ObjectId(sub.artifact_index_id.split('#')[1]))
+        if not merge_request:
+            print 'Could not find merge request for %s' % sub
         new_title = 'Merge Request #%d: %s' % (merge_request.request_number, merge_request.summary)
         print '"%s" --> "%s"' % (sub.artifact_title , new_title)
         if(task != 'diff'):
             sub.artifact_title = new_title
+        session(sub).flush(sub)
 
-    ThreadLocalORMSession.flush_all()
 
 if __name__ == '__main__':
     main()
