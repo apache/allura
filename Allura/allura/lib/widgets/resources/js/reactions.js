c5774839b81a25db94d60f9e379ff9e91a993927
Shalitha Suranga <shalithasuranga@gmail.com>
2019-03-11 11:11:49 +0530

    [#8263] Interactive reaction list and source formatting

--- Allura/allura/lib/widgets/resources/js/reactions.js
+++ Allura/allura/lib/widgets/resources/js/reactions.js
@@ -19,76 +19,96 @@
 
 // Reaction support
 
-$(function(){
+$(function () {
 
-$('.reaction-button').tooltipster({
-    animation: 'fade',
-    delay: 200,
-    theme: 'tooltipster-default',
-    trigger: 'click',
-    position: 'top',
-    iconCloning: false,
-    maxWidth: 400,
-    contentAsHTML: true,
-    interactive: true,
-    functionReady: function(instance, helper) {
-        $(helper).find('.emoji_button').click(function() {
-            var r = $(this).data('emoji');
-            if($(this).hasClass('current')) {
-                $(this).removeClass('current');
-                $(instance).data('currentreact', '');
-            }
-            else {
-                $(this).addClass('current');
-                $(instance).data('currentreact', r);
-            }
-            reactComment(instance, r);
-        });
-    }
-});
-
-$('.reaction-button').each(function() {
-    var btnId = $(this).attr('id');
-    $(this).click(function(e) {
-        e.preventDefault();
-        var currentemoji = $(this).data('currentreact');
-        var emohtml = '';
-        for(var emo in global_reactions) {
-            emohtml += '<div class=\'emoji_button' + (currentemoji == emo ? ' current' : '') +  '\' data-emoji=\'' + emo + '\'>' + 
-            twemoji.parse(global_reactions[emo]) + '</div>';
+    $('.reaction-button').tooltipster({
+        animation: 'fade',
+        delay: 200,
+        theme: 'tooltipster-default',
+        trigger: 'click',
+        position: 'top',
+        iconCloning: false,
+        maxWidth: 400,
+        contentAsHTML: true,
+        interactive: true,
+        functionReady: function (instance, helper) {
+            $(helper).find('.emoji_button').click(function () {
+                var r = $(this).data('emoji');
+                if ($(this).hasClass('current')) {
+                    $(this).removeClass('current');
+                    $(instance).data('currentreact', '');
+                }
+                else {
+                    $(this).addClass('current');
+                    $(instance).data('currentreact', r);
+                }
+                reactComment(instance, r);
+            });
         }
-        var tooltiptext = '<div class="post-reactions-list">' + emohtml + '</div>';
-        $(this).tooltipster('content', tooltiptext);
-        $(this).tooltipster('show');
     });
+
+    $('.reaction-button').each(function () {
+        var btnId = $(this).attr('id');
+        $(this).click(function (e) {
+            e.preventDefault();
+            var currentemoji = $(this).data('currentreact');
+            var emohtml = '';
+            for (var emo in global_reactions) {
+                emohtml += '<div class=\'emoji_button' + (currentemoji == emo ? ' current' : '') + '\' data-emoji=\'' + emo + '\'>' +
+                    twemoji.parse(global_reactions[emo]) + '</div>';
+            }
+            var tooltiptext = '<div class="post-reactions-list">' + emohtml + '</div>';
+            $(this).tooltipster('content', tooltiptext);
+            $(this).tooltipster('show');
+        });
+    });
+
+    attachClickEvents($('.reactions .reaction'));
+
 });
 
 
-});
+function attachClickEvents(reactionList) {
+    reactionList.each(function () {
+        $(this).click(function (e) {
+            var react_button = $(this).closest('.post-content').find('.reaction-button');
+            var r = $(this).data('react');
+            if ($(this).hasClass('reaction-current'))
+                $(react_button).data('currentreact', '');
+            else 
+                $(react_button).data('currentreact', r);
+            
+            reactComment(react_button, r);
+        });
+    });
+}
 
-function reactComment(btn ,r) {
+function reactComment(btn, r) {
     var reacts_list = btn.closest('.post-content').find('.reactions');
     var currentemoji = $(btn).data('currentreact');
     $.ajax({
         type: 'post',
         url: btn.data('commentlink') + 'post_reaction',
         data: {
-            'r' : r,
-            '_session_id' : $.cookie('_session_id')
+            'r': r,
+            '_session_id': $.cookie('_session_id')
         },
-        success: function(res) {
+        success: function (res) {
             var react_html = '';
 
             for (var i in res.counts) {
-                react_html += '<div class="reaction' + (currentemoji == i ? ' reaction-current' : '') + '">' +
-                                    '<div class="emoj">' + global_reactions[i] +'</div>' + 
-                                    '<div class="emoj-count">' + res.counts[i] + '</div>' + 
-                                '</div>';
+                react_html += '<div class="reaction' + (currentemoji == i ? ' reaction-current' : '') + '" data-react="' + i + '">' +
+                    '<div class="emoj">' + global_reactions[i] + '</div>' +
+                    '<div class="emoj-count">' + res.counts[i] + '</div>' +
+                    '</div>';
             }
-            
+
             reacts_list.html(react_html);
             twemoji.parse(reacts_list[0]);
             btn.tooltipster('hide');
+
+            // attach events to newly added dom
+            attachClickEvents($('.reaction', reacts_list));
         }
     });
 }
