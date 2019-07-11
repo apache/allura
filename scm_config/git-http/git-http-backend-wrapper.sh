709e4c18fe83cf91063d86de3d0128e4ada45975
Dave Brondsema <dave@brondsema.net>
2019-07-11 17:00:29 -0400

    [#8317] add license header to new file

--- scm_config/git-http/git-http-backend-wrapper.sh
+++ scm_config/git-http/git-http-backend-wrapper.sh
@@ -1,5 +1,22 @@
 #!/usr/bin/env bash
 
+#       Licensed to the Apache Software Foundation (ASF) under one
+#       or more contributor license agreements.  See the NOTICE file
+#       distributed with this work for additional information
+#       regarding copyright ownership.  The ASF licenses this file
+#       to you under the Apache License, Version 2.0 (the
+#       "License"); you may not use this file except in compliance
+#       with the License.  You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#       Unless required by applicable law or agreed to in writing,
+#       software distributed under the License is distributed on an
+#       "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+#       KIND, either express or implied.  See the License for the
+#       specific language governing permissions and limitations
+#       under the License.
+
 # This is so that git operations run as root, and can modify the scm repo files
 #
 # Up until git 2.11 and https://git.kernel.org/pub/scm/git/git.git/commit/?id=722ff7f876c8a2ad99c42434f58af098e61b96e8
