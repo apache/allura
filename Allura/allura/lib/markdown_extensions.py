8fb929c7aaa689621e4f5c8f4e5a9dd35927a19c
Dave Brondsema <dave@brondsema.net>
2019-08-21 13:41:02 -0400

    [#8325] upgrade markdown to 2.6.x

--- Allura/allura/lib/markdown_extensions.py
+++ Allura/allura/lib/markdown_extensions.py
@@ -515,7 +515,7 @@ class AutolinkPattern(markdown.inlinepatterns.Pattern):
         result.text = old_link
         # since this is run before the builtin 'escape' processor, we have to
         # do our own unescaping
-        for char in markdown.Markdown.ESCAPED_CHARS:
+        for char in self.markdown.ESCAPED_CHARS:
             old_link = old_link.replace('\\' + char, char)
         result.set('href', old_link)
         return result
