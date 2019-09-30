3ff817d366f5cbd0aa22c5413538aa850a95b822
Dave Brondsema <dave@brondsema.net>
2019-09-30 17:27:27 -0400

    [#8335] Strong search test

--- Allura/allura/tests/functional/test_search.py
+++ Allura/allura/tests/functional/test_search.py
@@ -14,9 +14,10 @@
 #       KIND, either express or implied.  See the License for the
 #       specific language governing permissions and limitations
 #       under the License.
-
 from mock import patch
+
 from allura.tests import TestController
+from allura.tests.decorators import with_tool
 
 
 class TestSearch(TestController):
@@ -28,6 +29,9 @@ class TestSearch(TestController):
         self.app.get('/search/', params=dict(q='Root'))
         assert search.called, search.called
 
+    # use test2 project since 'test' project has a subproject and MockSOLR can't handle "OR" (caused by subproject)
+    @with_tool('test2', 'Wiki', 'wiki')
     def test_project_search_controller(self):
-        self.app.get('/p/test/search/')
-        self.app.get('/p/test/search/', params=dict(q='Root'))
+        self.app.get('/p/test2/search/')
+        resp = self.app.get('/p/test2/search/', params=dict(q='wiki'))
+        resp.mustcontain('Welcome to your wiki! This is the default page')
