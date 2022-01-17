62f29636b1fa0ebf2c064b4a93e0f08c0cc33745
Dave Brondsema <dbrondsema@slashdotmedia.com>
2022-01-17 12:47:16 -0500

    [#8408] tricky parts of Markdown upgrade

--- Allura/allura/tests/test_helpers.py
+++ Allura/allura/tests/test_helpers.py
@@ -270,8 +270,9 @@ def test_render_any_markup_plain():
 
 
 def test_render_any_markup_formatting():
-    assert_equals(h.render_any_markup('README.md', '### foo\n'
-                                      '    <script>alert(1)</script> bar'),
+    # this is broken until markdown 3.1 fixes it again
+    assert_equals(str(h.render_any_markup('README.md', '### foo\n'
+                                          '    <script>alert(1)</script> bar')),
                   '<div class="markdown_content"><h3 id="foo">foo</h3>\n'
                   '<div class="codehilite"><pre><span></span><span class="nt">'
                   '&lt;script&gt;</span>alert(1)<span class="nt">'
