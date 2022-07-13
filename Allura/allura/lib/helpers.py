29a826c211df894a41a5da7b5825c4d9881270f7
Guillermo Cruz <guillermo.cruz@slashdotmedia.com>
2022-07-13 08:55:20 -0600

    [#8444] updated pagination_meta_tags with new args, added test and comments for querystring helper

--- Allura/allura/lib/helpers.py
+++ Allura/allura/lib/helpers.py
@@ -67,6 +67,7 @@ from allura.lib import exceptions as exc
 from allura.lib import utils
 import urllib.parse as urlparse
 from urllib.parse import urlencode
+import math
 from webob.multidict import MultiDict
 
 # import to make available to templates, don't delete:
@@ -155,9 +156,17 @@ def make_safe_path_portion(ustr, relaxed=True):
 def escape_json(data):
     return json.dumps(data).replace('<', '\\u003C')
 
-def querystring(request, data):
+def querystring(request, url_params):
+    """
+    add/update/remove url parameters. When a value is set to None the key will
+    be removed from the final constructed url.
+
+    :param request: request object
+    :param url_params: dict with the params that should be updated/added/deleted.
+    :return: a full url with updated url parameters.
+    """
     params = urlparse.parse_qs(request.query_string)
-    params.update(data)
+    params.update(url_params)
     for param in list(params.keys()):
         if params[param] is None:
             del params[param]
@@ -167,6 +176,8 @@ def querystring(request, data):
     url = url_parts._replace(query=urlencode(params)).geturl()
     return url
 
+def ceil(number):
+    return math.ceil(number)
 
 def strip_bad_unicode(s):
     """
