57e484eb0433c9eb80bc6692402d2b8fa14c1534
Dave Brondsema <dbrondsema@slashdotmedia.com>
2022-09-22 10:50:05 -0400

    [#8467] DefOptScriptTask and other improvements to task submission in web ui

--- Allura/allura/scripts/scripttask.py
+++ Allura/allura/scripts/scripttask.py
@@ -32,12 +32,26 @@ To use, subclass ScriptTask and implement two methods::
             '''Your main code goes here.'''
             pass
 
+Or using the `defopt library <https://defopt.readthedocs.io/>`_ (must be installed),
+subclass `DefOptScriptTask` and implement one method::
+
+    class MyScript(DefOptScriptTask):
+        @classmethod
+        def execute(cls, *, limit: int = 10, dry_run: bool = False):
+            '''
+            Description/usage of this script
+
+            :param limit:
+                Explanation of parametes, if desired
+            '''
+            pass
+
 To call as a script::
 
     if __name__ == '__main__':
         MyScript.main()
 
-To call as a task::
+To run as a background task::
 
     # post the task with cmd-line-style args
     MyScript.post('-p myproject --dry-run')
@@ -45,9 +59,9 @@ To call as a task::
 """
 
 import argparse
+import contextlib
+import io
 import logging
-
-import six
 
 from allura.lib.decorators import task
 from allura.lib.helpers import shlex_split
@@ -62,6 +76,7 @@ class MetaParserDocstring(type):
         return cls.parser().format_help()
 
     def __new__(meta, classname, bases, classDict):
+        # make it look like a task
         return task(type.__new__(meta, classname, bases, classDict))
 
 
@@ -70,6 +85,8 @@ class ScriptTask(metaclass=MetaParserDocstring):
     """Base class for a command-line script that is also executable as a task."""
 
     def __new__(cls, arg_string=''):
+        # when taskd calls SomeTaskClass(), then this runs.  Not really the normal way to use __new__
+        # and can't use __init__ since we want to return a value
         return cls._execute_task(arg_string)
 
     @classmethod
@@ -94,3 +111,49 @@ class ScriptTask(metaclass=MetaParserDocstring):
     def main(cls):
         options = cls.parser().parse_args()
         cls.execute(options)
+
+
+try:
+    import defopt
+except ModuleNotFoundError:
+    pass
+else:
+
+    class MetaDefOpt(type):
+        def __new__(meta, classname, bases, classDict):
+            return task(type.__new__(meta, classname, bases, classDict))
+
+        @property
+        def __doc__(cls):
+            with contextlib.redirect_stdout(io.StringIO()) as stderr:
+                try:
+                    cls.main(argv=['--help'])
+                except SystemExit:
+                    pass
+            return stderr.getvalue()
+
+
+    class DefOptScriptTask(metaclass=MetaDefOpt):
+        """Base class for a command-line script that is also executable as a task."""
+
+        def __new__(cls, arg_string=''):
+            # when taskd calls SomeTaskClass(), then this runs.  Not really the normal way to use __new__
+            # and can't use __init__ since we want to return a value
+            return cls._execute_task(arg_string)
+
+        @classmethod
+        def _execute_task(cls, arg_string):
+            try:
+                return cls.main(argv=shlex_split(arg_string or ''))
+            except SystemExit:
+                raise Exception("Error parsing args: '%s'" % arg_string)
+
+        @classmethod
+        def main(cls, **extra_kwargs):
+            return defopt.run(cls.execute, no_negated_flags=True, **extra_kwargs)
+
+        @classmethod
+        def execute(cls, *args, **kwargs):
+            """User code goes here, using defopt kwargs with type annotations"""
+            raise NotImplementedError
+
