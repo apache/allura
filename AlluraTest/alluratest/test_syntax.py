91d27e61a7bc286b57bb9d92970460a071f1cd47
Carlos Cruz <carlos.cruz@slashdotmedia.com>
2023-08-16 19:06:25 +0000

    [#8610] Add pre-commit hooks and clean up syntax.py tests to use them

--- AlluraTest/alluratest/test_syntax.py
+++ AlluraTest/alluratest/test_syntax.py
@@ -16,57 +16,29 @@
 #       under the License.
 
 import os.path
+import re
 from subprocess import Popen, PIPE
 import sys
 
-from unittest import SkipTest
-from itertools import zip_longest
+dir = os.path.abspath(os.path.dirname(__file__) + "/..")
 
-toplevel_dir = os.path.abspath(os.path.dirname(__file__) + "/../..")
-BASE_PATH = (toplevel_dir,) #freeze main path
 
-def run(cmd):
-    proc = Popen(cmd, shell=True, cwd=toplevel_dir, stdout=PIPE, stderr=PIPE)
-    # must capture & reprint stdout, so that pytest can capture it
+def run(cmd, dir=dir):
+    proc = Popen(cmd, shell=True, cwd=dir, stdout=PIPE, stderr=PIPE)
+    # must capture & reprint stdount, so that test suite can capture it
     (stdout, stderr) = proc.communicate()
-    sys.stdout.write(stdout.decode('utf-8'))
-    sys.stderr.write(stderr.decode('utf-8'))
-    return proc.returncode
+    stdout = stdout.decode('utf-8')
+    stderr = stderr.decode('utf-8')
+    if re.match(r'xargs: .* No such file', stderr):
+        raise Exception(stderr)
+    print(stdout, end='')
+    print(stderr, end='', file=sys.stderr)
+    return (proc.returncode, proc.communicate())
 
 
-find_py = r"find Allura Forge* -not -path '*/\.*' -name '*.py'"
-
-
-def test_no_local_tz_functions():
-    if run(find_py + r" | xargs grep '\.now(' ") not in [1, 123]:
-        raise Exception("These should use .utcnow()")
-    if run(find_py + r" | xargs grep '\.fromtimestamp(' ") not in [1, 123]:
-        raise Exception("These should use .utcfromtimestamp()")
-    if run(find_py + " | xargs grep 'mktime(' ") not in [1, 123]:
-        raise Exception("These should use calendar.timegm()")
-
-
-def test_no_prints():
-    skips = [
-        '/tests/',
-        'Allura/allura/command/',
-        'Allura/ldap-setup.py',
-        'Allura/ldap-userconfig.py',
-        '/scripts/',
-        'ForgeSVN/setup.py',
-    ]
-    if run(find_py + " | grep -v '" + "' | grep -v '".join(skips) + r"' | xargs grep -v '^ *#' | egrep -n '\bprint\(' | grep -E -v '(pprint|#pragma: ?printok)' ") != 1:
-        raise Exception("These should use logging instead of print")
-
-
-def test_no_tabs():
-    if run(find_py + " | xargs grep '	' ") not in [1, 123]:
-        raise Exception('These should not use tab chars')
-
-
-def test_ruff():
-    cmd = f"ruff check . --config {BASE_PATH[0]}/ruff.toml --show-source"
-    if run(cmd) != 0:
-        # print 'Command was: %s' % cmd
-        raise Exception('ruff failure, see stdout')
+def test_run_precommit():
+    cmd = "pre-commit run --all-files"
+    code, outputs = run(cmd, dir='/src/allura')
+    if code != 0:
+        raise Exception(f'pre-commit failed to run: {outputs[0].decode()} {outputs[1].decode()}')
 
