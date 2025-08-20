83ea15879905b94bb65f3258c9c1e61bd1455b67
Dave Brondsema <dbrondsema@slashdotmedia.com>
2025-08-20 12:48:44 -0400

    revert previous 2 commits about markdown, until we make a better implementation

--- Allura/allura/lib/utils.py
+++ Allura/allura/lib/utils.py
@@ -555,8 +555,7 @@ class ForgeHTMLSanitizerFilter(html5lib.filters.sanitizer.Filter):
 
         # srcset is used in our own project_list/project_summary widgets
         # which are used as macros so go through markdown
-        excluded_attributes = {(None, 'class'), (None, 'id')}
-        self.allowed_attributes = (html5lib.filters.sanitizer.allowed_attributes | {(None, 'srcset')}) - excluded_attributes
+        self.allowed_attributes = html5lib.filters.sanitizer.allowed_attributes | {(None, 'srcset')}
 
         self.valid_iframe_srcs = ('https://www.youtube.com/embed/',
                                   'https://www.youtube-nocookie.com/embed/',
