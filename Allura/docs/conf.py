d4cdde8c5b4a0393350c39300a480d8f0bd42feb
Dave Brondsema <dbrondsema@slashdotmedia.com>
2025-09-08 11:32:01 -0400

    remove a docs conf line that sphinx complained about

--- Allura/docs/conf.py
+++ Allura/docs/conf.py
@@ -116,7 +116,6 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
     import sphinx_rtd_theme
 
     html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # otherwise, readthedocs.org uses their theme by default, so no need to specify it
 
