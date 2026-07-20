c69bb5cd2be620f404ea653a2cff51d43a0ed124
Carlos Cruz <carlos.cruz@slashdotmedia.com>
2026-07-20 14:28:13 -0600

    [#8614] Add support for nested lists and tests to the field conversion script

---
+++ Allura/allura/tests/scripts/test_convert_encrypted_field.py
@@ -0,0 +1,111 @@
+#       Licensed to the Apache Software Foundation (ASF) under one
+#       or more contributor license agreements.  See the NOTICE file
+#       distributed with this work for additional information
+#       regarding copyright ownership.  The ASF licenses this file
+#       to you under the Apache License, Version 2.0 (the
+#       "License"); you may not use this file except in compliance
+#       with the License.  You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#       Unless required by applicable law or agreed to in writing,
+#       software distributed under the License is distributed on an
+#       "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+#       KIND, either express or implied.  See the License for the
+#       specific language governing permissions and limitations
+#       under the License.
+
+from ming import schema as S
+from ming.odm import FieldProperty
+
+from scripts.convert_encrypted_field import (
+    _encrypt_nested_array_value,
+    _remove_nested_array_value,
+    _schema_info_for_field_path,
+)
+
+
+class TestModel:
+    socialnetworks = FieldProperty([dict(
+        socialnetwork=str,
+        accounturl=str,
+        accounturl_encrypted=S.Binary,
+    )])
+
+    @classmethod
+    def encr(cls, value):
+        return None if value is None else f'encrypted:{value}'.encode()
+
+
+def test_schema_info_supports_nested_array_path():
+    field_schema, traverses_array = _schema_info_for_field_path(
+        TestModel, 'socialnetworks.accounturl_encrypted')
+
+    assert isinstance(field_schema, S.Binary)
+    assert traverses_array is True
+
+
+def test_encrypt_nested_array_value_updates_every_item():
+    socialnetworks = [
+        {'socialnetwork': 'Mastodon', 'accounturl': 'https://example.com/@user'},
+        {'socialnetwork': 'LinkedIn', 'accounturl': 'https://example.com/in/user'},
+        {'socialnetwork': 'Other', 'accounturl': None},
+    ]
+
+    encrypted, changed = _encrypt_nested_array_value(
+        TestModel,
+        socialnetworks,
+        ['accounturl'],
+        'accounturl_encrypted',
+        S.Binary(),
+        redo_all=False,
+    )
+
+    assert changed is True
+    assert encrypted == [
+        {
+            'socialnetwork': 'Mastodon',
+            'accounturl': 'https://example.com/@user',
+            'accounturl_encrypted': b'encrypted:https://example.com/@user',
+        },
+        {
+            'socialnetwork': 'LinkedIn',
+            'accounturl': 'https://example.com/in/user',
+            'accounturl_encrypted': b'encrypted:https://example.com/in/user',
+        },
+        {
+            'socialnetwork': 'Other',
+            'accounturl': None,
+            'accounturl_encrypted': None,
+        },
+    ]
+
+
+def test_remove_nested_array_value_preserves_encrypted_and_other_fields():
+    socialnetworks = [
+        {
+            'socialnetwork': 'Mastodon',
+            'accounturl': 'https://example.com/@user',
+            'accounturl_encrypted': b'encrypted:https://example.com/@user',
+        },
+        {
+            'socialnetwork': 'Other',
+            'accounturl': None,
+            'accounturl_encrypted': None,
+        },
+    ]
+
+    encrypted_only, changed = _remove_nested_array_value(
+        socialnetworks, ['accounturl'])
+
+    assert changed is True
+    assert encrypted_only == [
+        {
+            'socialnetwork': 'Mastodon',
+            'accounturl_encrypted': b'encrypted:https://example.com/@user',
+        },
+        {
+            'socialnetwork': 'Other',
+            'accounturl_encrypted': None,
+        },
+    ]
