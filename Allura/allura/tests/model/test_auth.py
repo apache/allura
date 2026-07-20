6eeb399a1c6d5989f5d54415a8421dfaa8d592b4
Carlos Cruz <carlos.cruz@slashdotmedia.com>
2026-07-20 12:19:07 -0600

    [#8614] Add backing encrypted fields for User and UserLoginDetails fields

--- Allura/allura/tests/model/test_auth.py
+++ Allura/allura/tests/model/test_auth.py
@@ -494,6 +494,94 @@ class TestAuth:
         assert raw_preferences['email_address_encrypted'] == M.User.encr('new@example.com')
         assert M.User.decr(raw_preferences['email_address_encrypted']) == 'new@example.com'
 
+    def test_personal_data_encrypted_fields_are_populated_on_creation(self):
+        user = M.User(
+            username='personal-data-encrypted-create-test',
+            last_access={
+                'login_ip': '192.0.2.1',
+                'session_ip': '198.51.100.1',
+            },
+            socialnetworks=[
+                {'socialnetwork': 'Mastodon', 'accounturl': 'https://example.com/@user'},
+                {'socialnetwork': 'Other', 'accounturl': None},
+            ],
+            telnumbers=['+1-555-0100', None],
+        )
+        ThreadLocalODMSession.flush_all()
+
+        document = state(user).document
+        assert document['last_access']['login_ip'] == '192.0.2.1'
+        assert document['last_access']['login_ip_encrypted'] == M.User.encr('192.0.2.1')
+        assert document['last_access']['session_ip'] == '198.51.100.1'
+        assert document['last_access']['session_ip_encrypted'] == M.User.encr('198.51.100.1')
+        assert document['socialnetworks'][0]['accounturl'] == 'https://example.com/@user'
+        assert document['socialnetworks'][0]['accounturl_encrypted'] == M.User.encr(
+            'https://example.com/@user')
+        assert document['socialnetworks'][1]['accounturl_encrypted'] is None
+        assert document['telnumbers'] == ['+1-555-0100', None]
+        assert user.telnumbers_encrypted == [M.User.encr('+1-555-0100'), None]
+
+    def test_personal_data_encrypted_fields_are_synced_on_update(self):
+        user = M.User(
+            username='personal-data-encrypted-update-test',
+            last_access={
+                'login_ip': '192.0.2.2',
+                'session_ip': '198.51.100.2',
+            },
+            socialnetworks=[
+                {'socialnetwork': 'Mastodon', 'accounturl': 'https://old.example/@user'},
+                {'socialnetwork': 'Other', 'accounturl': 'https://remove.example/user'},
+            ],
+            telnumbers=['+1-555-0101', '+1-555-0102'],
+        )
+        ThreadLocalODMSession.flush_all()
+
+        assert user.telnumbers_encrypted == [
+            M.User.encr('+1-555-0101'),
+            M.User.encr('+1-555-0102'),
+        ]
+        user.last_access['login_ip'] = '203.0.113.2'
+        user.last_access['session_ip'] = None
+        user.socialnetworks[0]['accounturl'] = 'https://new.example/@user'
+        user.socialnetworks.pop()
+        user.telnumbers.remove('+1-555-0101')
+        user.telnumbers.append('+1-555-0103')
+        ThreadLocalODMSession.flush_all()
+
+        document = state(user).document
+        assert document['last_access']['login_ip'] == '203.0.113.2'
+        assert document['last_access']['login_ip_encrypted'] == M.User.encr('203.0.113.2')
+        assert document['last_access']['session_ip'] is None
+        assert document['last_access']['session_ip_encrypted'] is None
+        assert len(document['socialnetworks']) == 1
+        assert document['socialnetworks'][0]['accounturl'] == 'https://new.example/@user'
+        assert document['socialnetworks'][0]['accounturl_encrypted'] == M.User.encr(
+            'https://new.example/@user')
+        assert document['telnumbers'] == ['+1-555-0102', '+1-555-0103']
+        assert user.telnumbers_encrypted == [
+            M.User.encr('+1-555-0102'),
+            M.User.encr('+1-555-0103'),
+        ]
+
+    def test_user_login_details_ip_encrypted_is_synced(self):
+        detail = M.UserLoginDetails(
+            user_id=c.user._id,
+            ip='192.0.2.3',
+            ua='PersonalDataEncryptionTest/1.0',
+        )
+        session(detail).flush(detail)
+
+        assert detail.ip == '192.0.2.3'
+        assert detail.ip_encrypted == M.UserLoginDetails.encr('192.0.2.3')
+        assert state(detail).document['ip'] == '192.0.2.3'
+
+        detail.ip = '203.0.113.3'
+        session(detail).flush(detail)
+
+        assert detail.ip == '203.0.113.3'
+        assert detail.ip_encrypted == M.UserLoginDetails.encr('203.0.113.3')
+        assert state(detail).document['ip'] == '203.0.113.3'
+
     def test_set_display_name_pref_updates_encrypted_field_and_cache(self):
         user = M.User(username='display-name-cache-test',
                       display_name='Original Display Name')
